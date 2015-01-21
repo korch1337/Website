4462f640ca7f53a85614269ceff19cd37c3bcd8b
korch1337 <felixkorch1@gmail.com>
2015-01-21 14:52:44 +0100

    Update serverinfo.php

--- html/layout/widgets/serverinfo.php
+++ html/layout/widgets/serverinfo.php
@@ -4,7 +4,7 @@
 * * * * * * <li><a href="onlinelist.php">Players Online: <?php echo user_count_online();?></a></li>
 * * * * * * <li>IP: <?php echo $_SERVER['SERVER_NAME']; ?></li>
 * * * * * * <li>Port: 7171</li>
-* * * * * * <li>Client: <a href="downloads.php">8.60</a></li>
+* * * * * * <li>Client: <a href="downloads.php">10.41</a></li>
 * * * * * * * * * * <!-- <img src="layout/images/offline.png" width="102" height="27"/> -->
 * * * * * * <!-- <li>Client: 9.52</li> -->
 * * </div>
