7924b4b12f20ac0f3a368da12a5d88cbd5b48a10
Nevnet1337 <davidcordesius@gmail.com>
2015-01-24 00:21:48 +0100

    Update whoisonline.php

--- html/layout/sub/whoisonline.php
+++ html/layout/sub/whoisonline.php
@@ -1,30 +1,56 @@
-<?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
+<?php require_once 'engine/init.php';
 
-<h1>Who is online?</h1>
+$records = mysql_select_single('SELECT `value` FROM `server_config` WHERE `config` = `players_record`');
+?>
+<img src="layout/images/titles/t_plaonline.png"/><p>
+<table>
+<tr class="yellow"><th>Server Status</th></tr>
+<tr><td><b><?php echo $config['site_title'] ?></b> record of players online: <?php echo $records['value'] ?>.</td></tr>
+<tr><td><?php
+                                $status = true;
+                                if ($config['status']['status_check']) {
+                                    @$sock = fsockopen ($config['status']['status_ip'], $config['status']['status_port'], $errno, $errstr, 1);
+                                    if(!$sock) {
+                                        echo '<b>';
+                                        echo $config['site_title'];
+                                        echo '</b> is currently offline.';
+                                        $status = false;
+                                    }
+                                    else {
+                                        $info = chr(6).chr(0).chr(255).chr(255).'info';
+                                        fwrite($sock, $info);
+                                        $data='';
+                                        while (!feof($sock))$data .= fgets($sock, 1024);
+                                        fclose($sock);
+                                        echo 'Currently ';
+                                        echo user_count_online();
+                                        echo ' players are online.';
+                                    }
+                                }
+?></td></tr>
+</table>
+<?php
 $array = online_list();
 if ($array) {
     ?>
    
-    <table id="onlinelistTable">
+    <table id="onlinelistTable" class="table table-striped table-hover">
         <tr class="yellow">
-            <td>Name:</td>
-            <td>Level:</td>
-            <td>Vocation:</td>
+            <th>Name</th>
+            <th>Level</th>
+            <th>Vocation</th>
         </tr>
             <?php
             foreach ($array as $value) {
-            $playerID = mysql_select_single("SELECT `id` FROM `players` WHERE `name` = '". $value[0] ."';");
-            $vocation = mysql_select_single("SELECT `value` FROM `player_storage` WHERE (`key` = 86228 AND `player_id` = '". $playerID['id'] ."');");
-           
-            if ($vocation['value'] == -1) { $vocationName = "Trainer"; }
-            elseif ($vocation['value'] == 3) { $vocationName = "Firetamer"; }
-            elseif ($vocation['value'] == 4) { $vocationName = "Waterdragon"; }
-            else { $vocationName = "Unknown Vocation"; }
-       
             echo '<tr>';
-            echo '<td><a href="characterprofile.php?name='. $value[0] .'">'. $value[0] .'</a></td>';
-            echo '<td>'. $value[1] .'</td>';
-            echo '<td>'. $vocationName .'</td>';
+            echo '<td><a href="characterprofile.php?name='. $value['name'] .'">'. $value['name'] .'</a></td>';
+            echo '<td>'. $value['level'] .'</td>';
+            echo '<td>'. vocation_id_to_name($value['vocation']) .'</td>';
             echo '</tr>';
             }
+            ?>
     </table>
+
+    <?php
+}
+?>
