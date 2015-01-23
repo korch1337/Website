391e70bc87d1b2d242987c660af7dd1752de9c5f
nevnet1337 <davidcordesius@gmail.com>
2015-01-23 22:40:06 +0100

    Revert if bad

--- html/layout/left_menu.php
+++ html/layout/left_menu.php
@@ -25,6 +25,22 @@
 					<br>
 				</div>
 
+				
+<div class="sidebar">
+	<h1><font size="5">Character Search</font></h1>
+	<div class="inner">
+		<li><font size="3">Character Name:<br></font>
+		<form type="submit" action="characterprofile.php" method="get">
+			<li><input type="text" name="name" class="search"><br><br>
+						<?php
+				/* Form file */
+				Token::create();
+			?>
+			<li><input type="submit" value="Search">
+		</form>
+	</div>
+</div>
+
 				<div class="sidebar" style="font-family: martel; font-size:17px">Home</div>
         		<ul class="inner"> 
                 	<li><a href="index.php">&nbsp;&raquo; News</a></li>
