edbc012c5cf8bea884e7bf51877e2469249659f7
Nevnet1337 <davidcordesius@gmail.com>
2015-01-24 02:27:02 +0100

    Update characterprofile.php

--- html/characterprofile.php
+++ html/characterprofile.php
@@ -107,6 +107,159 @@ if (isset($_GET['name']) === true && empty($_GET['name']) === false) {
 			<td><?php echo getClock($profile_znote_data['created'], true); ?></td></tr>
 			</table>
 			<!-- END CHARACTER INFORMATION -->
+			<?php
+/*/
+/   Znote AAC 1.4+ detailed character info (HP, MP, lvL, Exp, skills)
+/   HTML code based on code from Gesior
+/*/
+$tableWidth = 540;
+if ($config['TFSVersion'] != 'TFS_10') {
+    $playerData = mysql_select_multi("SELECT `value` FROM `player_skills` WHERE `player_id`='$user_id' LIMIT 7;");
+    $playerData['fist'] = $playerData[0]['value']; unset($playerData[0]);
+    $playerData['club'] = $playerData[1]['value']; unset($playerData[1]);
+    $playerData['sword'] = $playerData[2]['value']; unset($playerData[2]);
+    $playerData['axe'] = $playerData[3]['value']; unset($playerData[3]);
+    $playerData['dist'] = $playerData[4]['value']; unset($playerData[4]);
+    $playerData['shield'] = $playerData[5]['value']; unset($playerData[5]);
+    $playerData['fish'] = $playerData[6]['value']; unset($playerData[6]);
+
+    $player = mysql_select_single("SELECT `health`, `healthmax`, `mana`, `manamax`, `experience`, `maglevel`, `level` FROM `players` WHERE `id`='$user_id' LIMIT 1;");
+    $playerData['magic'] = $player['maglevel'];
+    $playerData['exp'] = array(
+        'now' => $player['experience'],
+        'next' => (int)(level_to_experience($player['level']+1) - $player['experience']),
+        'percent' => (int)(($player['experience'] - level_to_experience($player['level'])) / (level_to_experience($player['level']+1) - $player['experience']) * 100)
+    );
+    $playerData['health'] = array(
+        'now' => $player['health'],
+        'max' => $player['healthmax'],
+        'percent' => (int)($player['health'] / $player['healthmax'] * 100),
+    );
+    $playerData['mana'] = array(
+        'now' => $player['mana'],
+        'max' => $player['manamax'],
+        'percent' => (int)($player['mana'] / $player['manamax'] * 100),
+    );
+} else {
+    $player = mysql_select_single("SELECT `health`, `healthmax`, `mana`, `manamax`, `experience`, `skill_fist`, `skill_club`, `skill_sword`, `skill_axe`, `skill_dist`, `skill_shielding`, `skill_fishing`, `maglevel`, `level` FROM `players` WHERE `id`='$user_id' LIMIT 1;");
+    $playerData = array(
+        'fist' => $player['skill_fist'],
+        'club' => $player['skill_club'],
+        'sword' => $player['skill_sword'],
+        'axe' => $player['skill_axe'],
+        'dist' => $player['skill_dist'],
+        'shield' => $player['skill_shielding'],
+        'fish' => $player['skill_fishing'],
+        'magic' => $player['maglevel'],
+        'exp' => array(
+            'now' => $player['experience'],
+            'next' => (int)(level_to_experience($player['level']+1) - $player['experience']),
+            'percent' => (int)(($player['experience'] - level_to_experience($player['level'])) / (level_to_experience($player['level']+1) - $player['experience']) * 100)
+        ),
+        'health' => array(
+            'now' => $player['health'],
+            'max' => $player['healthmax'],
+            'percent' => (int)($player['health'] / $player['healthmax'] * 100),
+        ),
+        'mana' => array(
+            'now' => $player['mana'],
+            'max' => $player['manamax'],
+            'percent' => (int)($player['mana'] / $player['manamax'] * 100),
+        )
+    );
+}
+// Incase they have more health/mana than they should due to equipment bonus etc
+if ($playerData['exp']['percent'] > 100) $playerData['exp']['percent'] = 100;
+if ($playerData['health']['percent'] > 100) $playerData['health']['percent'] = 100;
+if ($playerData['mana']['percent'] > 100) $playerData['mana']['percent'] = 100;
+//data_dump($playerData, false, "Player Data");
+?>
+<!-- PLAYER SKILLS TABLE -->
+<table cellspacing="1" cellpadding="4" style="width: <?php echo $tableWidth; ?>px;">
+    <tr class="yellow">
+        <th>Fist</th>
+        <th>Club</th>
+        <th>Sword</th>
+        <th>Axe</th>
+        <th>Dist</th>
+        <th>Shield</th>
+        <th>Fish</th>
+        <th>Magic</th>
+    </tr>
+    <tr>
+        <td bgcolor="#D4C0A1" align="left">
+            <?php echo $playerData['fist']; ?>
+        </td>
+        <td bgcolor="#D4C0A1" align="left">
+            <?php echo $playerData['club']; ?>
+        </td>
+        <td bgcolor="#D4C0A1" align="left">
+            <?php echo $playerData['sword']; ?>
+        </td>
+        <td bgcolor="#D4C0A1" align="left">
+            <?php echo $playerData['axe']; ?>
+        </td>
+        <td bgcolor="#D4C0A1" align="left">
+            <?php echo $playerData['dist']; ?>
+        </td>
+        <td bgcolor="#D4C0A1" align="left">
+            <?php echo $playerData['shield']; ?>
+        </td>
+        <td bgcolor="#D4C0A1" align="left">
+            <?php echo $playerData['fish']; ?>
+        </td>
+        <td bgcolor="#D4C0A1" align="left">
+            <?php echo $playerData['magic']; ?>
+        </td>
+    </tr>
+</table>
+<!-- PLAYER INFO TABLE -->
+<table cellspacing="1" cellpadding="4" style="width: <?php echo $tableWidth; ?>px;">
+    <tr>
+        <td bgcolor="#F1E0C6" align="left" width="20%">
+            <b>Player HP:</b>
+        </td>
+        <td bgcolor="#F1E0C6" align="left">
+            <?php echo $playerData['health']['now'].'/'.$playerData['health']['max']; ?>
+            <div style="width: 100%; height: 3px; border: 1px solid #000;">
+                <div style="background: red; width: <?php echo $playerData['health']['percent']; ?>%; height: 3px;">
+                </div>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#D4C0A1" align="left">
+            <b>Player MP:</b>
+        </td>
+        <td bgcolor="#D4C0A1" align="left">
+            <?php echo $playerData['mana']['now'].'/'.$playerData['mana']['max']; ?>
+            <div style="width: 100%; height: 3px; border: 1px solid #000;">
+                <div style="background: blue; width: <?php echo $playerData['mana']['percent']; ?>%; height: 3px;">
+                </div>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#D4C0A1" align="left">
+            <b>Player XP:</b>
+        </td>
+        <td bgcolor="#D4C0A1" align="left">
+            <?php echo number_format($playerData['exp']['now'], 0, "", " "); ?> Experience.
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#F1E0C6" align="left">
+            <b>To Next Lvl:</b>
+        </td>
+        <td bgcolor="#F1E0C6" align="left">
+            Need <b><?php echo number_format($playerData['exp']['next'], 0, "", " "); ?> experience (<?php echo 100-$playerData['exp']['percent']; ?>%)</b> to Level <b><?php echo $player['level']+1; ?></b>.
+            <div title="99.320604545 %" style="width: 100%; height: 3px; border: 1px solid #000;">
+                <div style="background: red; width: <?php echo $playerData['exp']['percent']; ?>%; height: 3px;"></div>
+            </div>
+        </td>
+    </tr>
+</table>
+<!-- END detailed character info -->
 			<!-- DEATH LIST -->
 					<?php
 					if ($config['TFSVersion'] == 'TFS_02') {
@@ -236,6 +389,269 @@ if (isset($_GET['name']) === true && empty($_GET['name']) === false) {
                                 }
                                 ?>
                                 <!-- END QUEST PROGRESSION -->
+                                <?php
+/*/
+/   Player character profile EQ shower
+/   Based on code from CorneX
+/   Written to Znote AAC by Znote.
+/   Should work on all TFS versions.
+/   Znote AAC 1.4+
+/*/
+
+// Item image server
+$imageServer = $config['shop']['imageServer'];
+$imageType = $config['shop']['imageType'];
+if (count($imageType) < 3) $imageType = 'gif';
+// Fetch player equipped data
+$PEQD = mysql_select_multi("SELECT `player_id`, `pid`, `itemtype`, `count` FROM `player_items` WHERE `player_id`='$user_id' AND `pid`<'11'");
+// If player have equipped items
+if ($PEQD !== false) {
+    // PEQD = Player EQ Data
+    $PEQ = array(
+        1 => false,
+        2 => false,
+        3 => false,
+        4 => false,
+        5 => false,
+        6 => false,
+        7 => false,
+        8 => false,
+        9 => false,
+        10 => false,
+    );
+    // Fill player equipments array with fetched data results (PEQD)
+    foreach ($PEQD as $EQ) $PEQ[$EQ['pid']] = "http://{$imageServer}/".$EQ['itemtype'].".{$imageType}";
+    ?>
+    <!-- Fix item positions CSS -->
+    <style type="text/css">
+    /* CSS by CorneX */
+    .signBgrnd {
+        background-image:url('eq/outfit.png');
+        background-repeat:no-repeat;
+        position:relative;
+        float: right;
+        margin:10px;
+        padding:10px;
+        height:153px;
+        width:118px;
+    }
+    /* Weapon */
+    .signBgrnd .wep {
+        position: absolute;
+        top: 56px;
+        left: 6px;
+        width: 32px;
+        height: 32px;
+    }
+    .signBgrnd .wep img {
+        background-image:url('eq/bg.png');
+        max-width: 100%;
+    }
+    /* Armor */
+    .signBgrnd .arm {
+        position: absolute;
+        top: 41px;
+        left: 43px;
+        width: 32px;
+        height: 32px;
+    }
+    .signBgrnd .arm img {
+        background-image:url('eq/bg.png');
+        max-width: 100%;
+    }
+    /* Helmet */
+    .signBgrnd .helm {
+        position: absolute;
+        top: 5px;
+        left: 43px;
+        width: 32px;
+        height: 32px;
+    }
+    .signBgrnd .helm img {
+        background-image:url('eq/bg.png');
+        max-width: 100%;
+    }
+    /* legs */
+    .signBgrnd .legs {
+        position: absolute;
+        top: 79px;
+        left: 43px;
+        width: 32px;
+        height: 32px;
+    }
+    .signBgrnd .legs img {
+        background-image:url('eq/bg.png');
+        max-width: 100%;
+    }
+    /* boots */
+    .signBgrnd .boots {
+        position: absolute;
+        top: 116px;
+        left: 43px;
+        width: 32px;
+        height: 32px;
+    }
+    .signBgrnd .boots img {
+        background-image:url('eq/bg.png');
+        max-width: 100%;
+    }
+    /* ring */
+    .signBgrnd .ring {
+        position: absolute;
+        top: 93px;
+        left: 6px;
+        width: 32px;
+        height: 32px;
+    }
+    .signBgrnd .ring img {
+        background-image:url('eq/bg.png');
+        max-width: 100%;
+    }
+    /* amulet */
+    .signBgrnd .amulet {
+        position: absolute;
+        top: 20px;
+        left: 7px;
+        width: 32px;
+        height: 32px;
+    }
+    .signBgrnd .amulet img {
+        background-image:url('eq/bg.png');
+        max-width: 100%;
+    }
+    /* backpack */
+    .signBgrnd .backpack {
+        position: absolute;
+        top: 20px;
+        left: 80px;
+        width: 32px;
+        height: 32px;
+    }
+    .signBgrnd .backpack img {
+        background-image:url('eq/bg.png');
+        max-width: 100%;
+    }
+    /* shield */
+    .signBgrnd .shield {
+        position: absolute;
+        top: 56px;
+        left: 80px;
+        width: 32px;
+        height: 32px;
+    }
+    .signBgrnd .shield img {
+        background-image:url('eq/bg.png');
+        max-width: 100%;
+    }
+    /* arrow */
+    .signBgrnd .arrow {
+       position: absolute;
+       top: 93px;
+       left: 80px;
+       width: 32px;
+       height: 32px;
+    }
+    .signBgrnd .arrow img {
+        background-image:url('eq/bg.png');
+        max-width: 100%;
+    }
+    </style>
+    <!-- Render HTML -->
+    <div class="signBgrnd">
+        <div class="helm">
+            <?php
+            if ($PEQ[1] != false) {
+                ?>
+                <img src="<?php echo $PEQ[1]; ?>" alt="Image of player helmet">
+                <?php
+            }
+            ?>
+        </div>
+        <div class="amulet">
+            <?php
+            if ($PEQ[2] != false) {
+                ?>
+                <img src="<?php echo $PEQ[2]; ?>" alt="Image of player amulet">
+                <?php
+            }
+            ?>
+        </div>
+        <div class="wep">
+            <?php
+            if ($PEQ[6] != false) {
+                ?>
+                <img src="<?php echo $PEQ[6]; ?>" alt="Image of player left hand">
+                <?php
+            }
+            ?>
+        </div>
+        <div class="ring">
+            <?php
+            if ($PEQ[9] != false) {
+                ?>
+                <img src="<?php echo $PEQ[9]; ?>" alt="Image of player ring">
+                <?php
+            }
+            ?>
+        </div>
+        <div class="arm">
+            <?php
+            if ($PEQ[4] != false) {
+                ?>
+                <img src="<?php echo $PEQ[4]; ?>" alt="Image of player armor">
+                <?php
+            }
+            ?>
+        </div>
+        <div class="legs">
+            <?php
+            if ($PEQ[7] != false) {
+                ?>
+                <img src="<?php echo $PEQ[7]; ?>" alt="Image of player legs">
+                <?php
+            }
+            ?>
+        </div>
+        <div class="boots">
+            <?php
+            if ($PEQ[8] != false) {
+                ?>
+                <img src="<?php echo $PEQ[8]; ?>" alt="Image of player boots">
+                <?php
+            }
+            ?>
+        </div>
+        <div class="backpack">
+            <?php
+            if ($PEQ[3] != false) {
+                ?>
+                <img src="<?php echo $PEQ[3]; ?>" alt="Image of player backpack">
+                <?php
+            }
+            ?>
+        </div>
+        <div class="shield">
+            <?php
+            if ($PEQ[5] != false) {
+                ?>
+                <img src="<?php echo $PEQ[5]; ?>" alt="Image of player shield">
+                <?php
+            }
+            ?>
+        </div>
+        <div class="arrow">
+            <?php
+            if ($PEQ[10] != false) {
+                ?>
+                <img src="<?php echo $PEQ[10]; ?>" alt="Image of player arrow">
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+    <?php
+}
+?>    <!-- END EQ SHOWER -->
 				<!-- CHARACTER LIST -->
 				<?php
 				if (user_character_hide($profile_data['name']) != 1) {
