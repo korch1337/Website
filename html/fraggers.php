0e184283da73b9eb6c7aee9e68fc35632004da6d
Nevnet1337 <davidcordesius@gmail.com>
2015-01-25 16:21:46 +0100

    Update fraggers.php

--- html/fraggers.php
+++ html/fraggers.php
@@ -1,32 +1,22 @@
-<?php
-require_once 'engine/init.php';
-include 'layout/overall/header.php';
-?>
-<h2>Top Fraggers</h2>
 <?php 
- 
+if(!defined('INITIALIZED'))
+    exit;
 
-$i = 0; 
-$ee = mysql_query("SELECT `p`.`name` AS `name`, COUNT(`p`.`name`) as `frags` 
- FROM `killers` k 
- LEFT JOIN `player_killers` pk ON `k`.`id` = `pk`.`kill_id` 
- LEFT JOIN `players` p ON `pk`.`player_id` = `p`.`id` 
-WHERE `k`.`unjustified` = 1 AND `k`.`final_hit` = 1 
- GROUP BY `name` 
- ORDER BY `frags` DESC, `name` ASC 
- LIMIT 0,30;")or die(mysql_error());
- echo '<table><tr class="yellow"><td>Name</td><td>Frags</td></tr>';
-while($e = mysql_fetch_assoc($ee)) 
+$main_content .= '<div style="text-align: center; font-weight: bold;">Top 30 fraggers on ' . htmlspecialchars($config['server']['serverName']) . '</div>
+<table border="0" cellspacing="1" cellpadding="4" width="100%">
+    <tr bgcolor="' . $config['site']['vdarkborder'] . '">
+        <td class="white" style="text-align: center; font-weight: bold;">Name</td>
+        <td class="white" style="text-align: center; font-weight: bold;">Frags</td>
+    </tr>';
 
-{ 
- $i++; 
- echo 
- 
- '<tr><td>'.$e['name'] .'</td> <td> '.$e['frags'].'</tr><br>'; 
-} 
-echo '</table>'; 
-?>
-<?php
+$i = 0;
+foreach($SQL->query('SELECT `killed_by` as `name`, COUNT(`killed_by`) AS `frags` FROM `player_deaths` WHERE `is_player` = 1 GROUP BY `killed_by` ORDER BY COUNT(`killed_by`) DESC LIMIT 0, 30;') as $player)
+{
+    $i++;
+    $main_content .= '<tr bgcolor="' . (is_int($i / 2) ? $config['site']['lightborder'] : $config['site']['darkborder']) . '">
+        <td><a href="?subtopic=characters&name=' . urlencode($player['name']) . '">' . htmlspecialchars($player['name']) . '</a></td>
+        <td style="text-align: center;">' . $player['frags'] . '</td>
+    </tr>';
+}
 
-include 'layout/overall/footer.php';
-?>
+$main_content .= '</table>';
