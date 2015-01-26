bd347bfdc3ae2eca7091cdaa095c43d480dfaf4a
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 06:17:44 +0100

    Update right_menu.php

--- html/layout/right_menu.php
+++ html/layout/right_menu.php
@@ -1,7 +1,6 @@
 			<div class="right">
 			
 				<div class="sidebar" style="font-family: martel; font-size:17px">Shop</div>
-				<ul class="inner"> 
 					<li><a href="buypoints.php">&nbsp;&raquo;Buy Points</a></li>
 					<li><a href="shop.php">&nbsp;&raquo;Shop Offers</a></li>
 					  <?php include 'streamtemple.php'; ?>
