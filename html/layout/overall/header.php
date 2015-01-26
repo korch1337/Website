5b30b748395ddf5cb80f832bcfb2605d949ad77b
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 22:21:28 +0100

    Update header.php

--- html/layout/overall/header.php
+++ html/layout/overall/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+<?php include('streamtemple.php')?>
 <?php
 $time = microtime();
 $time = explode(' ', $time);
@@ -19,7 +20,6 @@ $start = $time;
 </head>
 <body>
 <div class="m_position"> 
-	<?php include('streamtemple.php')?>
 	<img src="layout/images/head.png"/>
 		<div class="m_center">
 			<?php include('layout/left_menu.php'); ?>
