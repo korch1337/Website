d604611e9beb393b330c91341490819d90f54efc
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 06:05:29 +0100

    Update right_menu.php

--- html/layout/right_menu.php
+++ html/layout/right_menu.php
@@ -1,29 +1,5 @@
 			<div class="right">
-				<div class="m_status" align="center">Server Status</div>
-				<div class="m_online" align="center">
-					<b>Status:  
-						<?php
-							$status = true;
-							if ($config['status']['status_check']) {
-								@$sock = fsockopen ($config['status']['status_ip'], $config['status']['status_port'], $errno, $errstr, 1);
-								if(!$sock) {
-									echo '<font color="#9873DA"> Offline</font>';
-									$status = false;
-								}
-								else {
-									$info = chr(6).chr(0).chr(255).chr(255).'info';
-									fwrite($sock, $info);
-									$data='';
-									while (!feof($sock))$data .= fgets($sock, 1024);
-									fclose($sock);
-									echo '<font color="#9873DA"> Online</font><br><a href="whoisonline.php">';
-									echo user_count_online();
-									echo ' players online<br/></a>';
-								}
-							}
-						?></b>
-					<br>
-				</div>
+			
 
 				
 		<div class="sidebar" style="font-family: martel; font-size:17px">Search</div>
