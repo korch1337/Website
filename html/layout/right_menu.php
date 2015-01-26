b2d03403311ba7e35ca92356f298db125278627b
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 06:00:08 +0100

    Update right_menu.php

--- html/layout/right_menu.php
+++ html/layout/right_menu.php
@@ -1,7 +1,7 @@
 			<div class="right">
 				
 
-				<div class="sidebar" style="font-family: martel; font-size:17px">Community</div>
+				<div class="sidebar" style="font-family: martel; font-size:17px"></div>
 				<ul class="inner">
 				<?php	include 'streamtemple.php'; ?>
 				</ul>
