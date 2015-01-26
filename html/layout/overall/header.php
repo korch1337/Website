04a57c2f4bc5d8798dcc196033a62c3976ec2571
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 07:51:15 +0100

    Update header.php

--- html/layout/overall/header.php
+++ html/layout/overall/header.php
@@ -22,8 +22,8 @@ $start = $time;
 <div class="m_position"> 
 	<img src="layout/images/logo.png" alt="<?PHP echo $config['site_title'];?> logo"/>
 	<img src="layout/images/head.png"/>
+	<?php include('layout/right_menu.php'); ?>
 		<div class="m_center">
-<?php include('layout/right_menu.php'); ?>
 			<?php include('layout/left_menu.php'); ?>
 			
 			<div class="m_content">
