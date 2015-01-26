a943cf4a4e79e34908bf704314925de8453f7748
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 07:58:15 +0100

    Update footer.php

--- html/layout/overall/footer.php
+++ html/layout/overall/footer.php
@@ -2,9 +2,7 @@
 </script>
 		</div>
 	<div style="clear:both;">	</div>
-	<div class:"right">
-	<?php include('layout/right_menu.php'); ?>
-	</div>
+	<?php include 'images/foot.png' ?>
 	<div id="m_footer">
 		<center><p>Guilcera Layout Peonso. Engine <a href="credits.php">Znote AAC</a><br>Tibia copyrighted by CipSoft GmbH.<br></p></center>
 	</div>
