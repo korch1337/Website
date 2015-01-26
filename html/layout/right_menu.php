5460562815cfa412e999e4035fe1d42577d7f6eb
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 06:47:21 +0100

    Update right_menu.php

--- html/layout/right_menu.php
+++ html/layout/right_menu.php
@@ -1,4 +1,10 @@
-    <div class="sidebar" style=""></div>
+
+ 
+	
+<link rel="stylesheet" type="text/css" href="layout/main.css" />
+
+    <div class="right">
     <ul class="inner">
     <?php include 'streamtemple.php'; ?>
     </ul>
+ 
