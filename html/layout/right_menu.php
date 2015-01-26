596c799488000a0331055417cd68ff30b31a95b5
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 07:36:01 +0100

    Update right_menu.php

--- html/layout/right_menu.php
+++ html/layout/right_menu.php
@@ -1,18 +1,7 @@
 <div class="right">
     
 
-    <div class="sidebar" style=""></div>
-    <ul class="inner">
     <?php include 'streamtemple.php'; ?>
-    </ul>
-   
-     <!-- <div class="sidebar" style="font-family: martel; font-size:17px">Library</div>
-     <ul class="inner"> 
-      <li><a href="sub.php?page=map">&nbsp;&raquo; Map</a></li>
-      <li><a href="sub.php?page=monsters">&nbsp;&raquo; Monsters</a></li>
-      <li><a href="sub.php?page=experiencetable">&nbsp;&raquo; Experience</a></li>
-    </ul> -->
-    
-
+ 
     
    </div>
