2673030fddf344d866a30bf267cc16e04bce3ebf
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 06:33:39 +0100

    Update right_menu.php

--- html/layout/right_menu.php
+++ html/layout/right_menu.php
@@ -1,12 +1,9 @@
 
+<div class="right">
 
-	<link rel="stylesheet" type="text/css" href="main.css">
-	<div class="right">
-	<style>
 	{
 	background: url(<?php include'streamtemple.php'; displayBackground();?>); 
 	}
-	</style>
 	
 
 
