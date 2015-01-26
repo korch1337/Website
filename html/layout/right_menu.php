9babdd37dfd44a4ee5ccdc2811ee49a3a004e226
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 06:30:18 +0100

    Update right_menu.php

--- html/layout/right_menu.php
+++ html/layout/right_menu.php
@@ -6,10 +6,6 @@
 	body
 	{
 	background: url(<?php include'streamtemple.php'; displayBackground();?>) no-repeat center center fixed; 
-	-webkit-background-size: cover;
-	-moz-background-size: cover;
-	-o-background-size: cover;
-	background-size: cover;
 	}
 	</style>
 	<title></title>
