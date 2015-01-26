ebd4aef62135d2e00486cb7873eec705973785ce
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 21:50:51 +0100

    Update right_menu.php

--- html/layout/right_menu.php
+++ html/layout/right_menu.php
@@ -1,17 +1,11 @@
-			<link rel="stylesheet" type="text/css" href="main.css">
-			<div class="right">
-			
-	
-	
-
-					<li><a href="market.php">&nbsp;&raquo; Item Market</a></li>
-					<li><a href="gallery.php">&nbsp;&raquo; Gallery</a></li>
-					<li><a href="deaths.php">&nbsp;&raquo; Deaths</a></li>
-					<li><a href="killers.php">&nbsp;&raquo; Killers</a></li>
-					<li><a href="whoisonline.php">&nbsp;&raquo; Players Online</a></li>
-					<li><a href="highscores.php">&nbsp;&raquo; Highscores</a></li>
-					<li><a href="houses.php">&nbsp;&raquo; Houses</a></li>
-					<li><a href="guilds.php">&nbsp;&raquo; Guilds</a></li>
-					<li><a href="guildwar.php">&nbsp;&raquo; Guild Wars</a></li>
-			
-			</div> 
+<script type="text/javascript">
+    function doAjax() {
+        $.ajax({
+            url: "some-json-data.js",
+            context: document.body,
+            success: function(responseText) {
+                var json = eval("(" + responseText + ")");
+            }
+        });
+    };
+</script>
