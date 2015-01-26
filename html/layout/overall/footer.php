8db70deb286c8bc13bacd924894816579e2f612a
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 06:41:50 +0100

    Update footer.php

--- html/layout/overall/footer.php
+++ html/layout/overall/footer.php
@@ -3,6 +3,7 @@
 		</div>
 	<div style="clear:both;"></div>
 	<div id="m_footer">
+	<?php include('layout/right_menu.php'); ?>
 		<center><p>Guilcera Layout Peonso. Engine <a href="credits.php">Znote AAC</a><br>Tibia copyrighted by CipSoft GmbH.<br></p></center>
 	</div>
 </div>				
