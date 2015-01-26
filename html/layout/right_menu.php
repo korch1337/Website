88f1972998ce2ad98f9943729eb5be440fee3e1a
Nevnet1337 <davidcordesius@gmail.com>
2015-01-26 06:28:18 +0100

    Update right_menu.php

--- html/layout/right_menu.php
+++ html/layout/right_menu.php
@@ -1,7 +1,19 @@
-			<div class="right">
-			<div class="m_status" align="center">Server Status</div>
+
+	<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<style>
+	body
+	{
+	background: url(<?php include'streamtemple.php'; displayBackground();?>) no-repeat center center fixed; 
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+	}
+	</style>
+	<title></title>
+</head>
 			
 				
 			
 				
-			</div> 
