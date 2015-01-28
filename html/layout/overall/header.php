3e6ed2a109648a4f697e6bd55abdd68c02f59d50
Nevnet1337 <davidcordesius@gmail.com>
2015-01-28 19:52:46 +0100

    Update header.php

--- html/layout/overall/header.php
+++ html/layout/overall/header.php
@@ -18,11 +18,13 @@ $start = $time;
 <link href="layout/favicon.ico" rel="shortcut icon" />
 </head>
 <body>
+<div class="streamoverlay">
+<?php include('streamtemple.php'); ?>
+</div>
 <div class="m_position"> 
 	<img src="layout/images/logo.png" alt="<?PHP echo $config['site_title'];?> logo"/>
 	<img src="layout/images/head.png"/>
 
 		<div class="m_center">
 			<?php include('layout/left_menu.php'); ?>
-			<?php include('streamtemple.php'); ?>
 			<div class="m_content">
