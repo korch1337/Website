3af8357b229b4492ab0dfc4e109fdcb93ef8de97
Nevnet1337 <davidcordesius@gmail.com>
2015-01-29 20:03:43 +0100

    Update bountyhunters.php

--- html/bountyhunters.php
+++ html/bountyhunters.php
@@ -1,13 +1,12 @@
 <?php include 'layout/overall/header.php';
 
-<head>
-<h1>Latest Deaths</h1>
+echo'
+<h1>Latest Bounties</h1>
 <table id="bountyTable" class="table table-striped">
 	<tr class="yellow">
 		<th>Bounty</th>
 		<th>Name</th>
 	</tr>
-</table>
-</head>
+</table>'
 
 include 'layout/overall/footer.php'; ?>
