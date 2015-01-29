e8bd57ad875a9ef764820e529b628c796962a524
Nevnet1337 <davidcordesius@gmail.com>
2015-01-29 21:04:49 +0100

    Update bountyhunters.php

--- html/bountyhunters.php
+++ html/bountyhunters.php
@@ -1,11 +1,10 @@
 <?php include 'layout/overall/header.php';
 
-
-	echo "<h2>Server stages</h2>
+echo "<h2>Server rates</h2>
 		<table class='table table-striped table-hover'>
-		<tbody><tr class='yellow'><td>Minium level</td><td>Maximun level</td><td>Multiplier</td></tr>";
-		// foreach loop här!! per <td>
-		echo '</tbody><table>';
+		<tbody><tr class='yellow'><td>Experience rate</td><td>Skills rate</td><td>Magic rate</td><td>Loot rate</td></tr>
+		<tr><td><center>x".$lua_path['rateExp']."</center></td><td><center>x".$lua_path['rateSkill']."</center></td><td><center>x".$lua_path['rateMagic']."</center></td><td><center>x".$lua_path['rateLoot']."</center></td></tr>
+		</tbody></table>";
 		
 		
 
