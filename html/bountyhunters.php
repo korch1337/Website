81c5506ff02cc21478470b5cccf2fc890691e67a
Nevnet1337 <davidcordesius@gmail.com>
2015-01-29 21:03:52 +0100

    Update bountyhunters.php

--- html/bountyhunters.php
+++ html/bountyhunters.php
@@ -1,19 +1,14 @@
-<?php require_once 'engine/init.php'; include 'layout/overall/header.php';
+<?php include 'layout/overall/header.php';
 
-echo '
-<table>
-  <tr>
-    <td>Customer Name</td>
-    <td>' '</td>
-  </tr>
-  <tr>
-    <td>Customer Age</td>
-    <td>' '</td>
-  </tr>
-  <tr>
-    <td>Customer ...</td>
-    <td>' '</td>
-  </tr>
-</table>';
+
+	echo "<h2>Server stages</h2>
+		<table class='table table-striped table-hover'>
+		<tbody><tr class='yellow'><td>Minium level</td><td>Maximun level</td><td>Multiplier</td></tr>";
+		// foreach loop här!! per <td>
+		echo '</tbody><table>';
+		
+		
+
 
 include 'layout/overall/footer.php'; ?>
+
