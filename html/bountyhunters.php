71ddd77da0d3d8ceeda76f464df3161ce2cd0ec8
Nevnet1337 <davidcordesius@gmail.com>
2015-01-29 22:15:42 +0100

    Update bountyhunters.php

--- html/bountyhunters.php
+++ html/bountyhunters.php
@@ -1,5 +1,5 @@
 <?php
-$main_content .= '
+$main_content .= 
     <TABLE BORDER=0 CELLPADDING=4 CELLSPACING=1 WIDTH=100%>
         <TR>
             <TD style="text-align:center;"><H2>Bounty Hunters</H2></TD>
@@ -16,8 +16,8 @@ $main_content .= '
 <br><b><font color="green">Money is added to your bank account automatically if you get a Bounty Kill.</font></b></TD>
         </TR> 
 </TABLE><br><br><table> 
-';
-$main_content .= '
+;
+$main_content .= 
         <TABLE BORDER=0 CELLSPACING=1 CELLPADDING=4 WIDTH=100%>
             <TR BGCOLOR="#505050">
                 <TD CLASS=white width=28%>
@@ -33,7 +33,7 @@ $main_content .= '
                 <TD CLASS=white width=28%>
                     <center><B>Killed by</B></center>
                 </TD>
-            </TR>';
+            </TR>;
 foreach($SQL->query('SELECT A.* , B.name AS hunted_by, C.name AS player_hunted, D.name AS killed_by
                         FROM bounty_hunters AS A
                         LEFT JOIN players AS B ON A.fp_id = B.id
@@ -48,14 +48,14 @@ foreach($SQL->query('SELECT A.* , B.name AS hunted_by, C.name AS player_hunted, 
         }
     $b = round($bounty[prize] / 1000000,2);
     $skill = $SQL->query('SELECT * FROM '.$SQL->tableName('players').' WHERE '.$SQL->fieldName('id').' = '.$bounty['sp_id'].'')->fetch();
-        $main_content .= '
+        $main_content .= 
                 <TR BGCOLOR="'.$color.'">
                     <TD><center><b><a href="?subtopic=characters&name='.$bounty['hunted_by'].'">'.$bounty['hunted_by'].'</a></b></center></TD>
                     <TD><center><b>'.$b.'  kk</b></center></TD>
                     <TD><center><b><a href="?subtopic=characters&name='.$bounty['player_hunted'].'">'.$bounty['player_hunted'].'</a></b></center></TD>
            <TD><div style="position: relative; width: 32px; height: 32px;"><div style="background-image: url(\'http://outfit-images.ots.me/outfit.php?id='.$skill['looktype'].'&addons='.$skill['lookaddons'].'&head='.$skill['lookhead'].'&body='.$skill['lookbody'].'&legs='.$skill['looklegs'].'&feet='.$skill['lookfeet'].'\'); position: absolute; width: 64px; height: 80px; background-position: bottom right; background-repeat: no-repeat; right: 0px; bottom: 0px;"></div></div></TD>
                     <TD><center><b>'.$killed_by.'</b></center></TD>
-                </TR>';
+                </TR>;
         $num++;
 }
 if($num == 0){
