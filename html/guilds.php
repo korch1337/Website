0ea7db3edb2e090138d9a34491570d6f7c99e954
Nevnet1337 <davidcordesius@gmail.com>
2015-01-30 21:22:09 +0100

    Update guilds.php

--- html/guilds.php
+++ html/guilds.php
@@ -66,7 +66,7 @@ if (isset($guilds) && !empty($guilds) && $guilds !== false) {
 					<img style="max-height: 100px; margin: auto; display: block;" src="<?php logo_exists($guild['name']); ?>">
 					</td>
 					<td>
-						<a href="forum.php?cat='. $category['id'] .'" /a>';
+						<a href="guilds.php?name='. $guild['name'] . '" /a>';
 						<b><?php echo $guild['name']; ?></b>
 						<?php if (strlen($guild['motd']) > 0) echo '<br>'.$guild['motd']; ?>
 					</td>
