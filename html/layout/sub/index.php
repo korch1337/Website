04905f26aa26cda59012d45b3a1de5c99b5f82c7
korch1337 <felixkorch1@gmail.com>
2015-01-30 20:30:21 +0100

    Update index.php

--- html/layout/sub/index.php
+++ html/layout/sub/index.php
@@ -2,7 +2,7 @@
 
 <?php echo '<table border="0" cellspacing="0"><tr class="yellow"><td><center>Server Information</center></td></tr> 
 <tr><td>'; 
-$getshit11 = mysql_query('SELECT `id`, `name` FROM `players` ORDER BY `id` DESC LIMIT 1');  
+$getshit11 = mysql_select_single('SELECT `id`, `name` FROM `players` ORDER BY `id` DESC LIMIT 1');  
 $fetchshitt = mysql_fetch_assoc($getshit11);  
 $getshit1 = mysql_query("SELECT COUNT(*) as `shit` FROM `players` "); 
 $fetchshit = mysql_fetch_assoc($getshit1); 
@@ -12,7 +12,7 @@ $getshit3 = mysql_query("SELECT COUNT(*) as `shiter` FROM `accounts` ");
 $fetchshit3 = mysql_fetch_assoc($getshit3); 
 $getshit4 = mysql_query("SELECT COUNT(*) as `yea` FROM `guilds` "); 
 $fetchshit4 = mysql_fetch_assoc($getshit4); 
-echo '<center>Welcome to our newest player: <a href="characterprofile.php?name='.$fetchshitt['name'].'">'.$fetchshitt['name'].'</a></center></td></tr>'; 
+echo '<center>Welcome to our newest player: <a href="characterprofile.php?name='.$getshit11['name'].'">'.$fetchshitt['name'].'</a></center></td></tr>'; 
 echo '<tr><td><center>The best player is: <a href="characterprofile.php?name='.$fetchshit2['name'].'">'.$fetchshit2['name'].'</a> level: '.$fetchshit2['level'].' congratulations!</center></td></tr>'; 
 echo '<tr><td><center>We have <b>'.$fetchshit3['shiter'].'</b> accounts in our database, <b>'.$fetchshit['shit'].'</b> players, and <b>'.$fetchshit4['yea'].' </b>guilds </center></td></tr>'; 
 echo '</table>'; ?>
