bcba3f6080a35051622340103cee9fc9bde2cf11
Nevnet1337 <davidcordesius@gmail.com>
2015-01-30 20:25:38 +0100

    Update index.php

--- html/layout/sub/index.php
+++ html/layout/sub/index.php
@@ -14,7 +14,7 @@ $getshit4 = mysql_query("SELECT COUNT(*) as `yea` FROM `guilds` ");
 $fetchshit4 = mysql_fetch_assoc($getshit4); 
 echo '<center>Welcome to our newest player: <a href="characterprofile.php?name='.$fetchshitt['name'].'">'.$fetchshitt['name'].'</a></center></td></tr>'; 
 echo '<tr><td><center>The best player is: <a href="characterprofile.php?name='.$fetchshit2['name'].'">'.$fetchshit2['name'].'</a> level: '.$fetchshit2['level'].' congratulations!</center></td></tr>'; 
-echo '<tr><td><center>We have <b>'. $fetchshit3 .'</b> accounts in our database, <b>'.$fetchshit['shit'].'</b> players, and <b>'.$fetchshit4['yea'].' </b>guilds </center></td></tr>'; 
+echo '<tr><td><center>We have <b>'.$getshit3['shiter'].'</b> accounts in our database, <b>'.$fetchshit['shit'].'</b> players, and <b>'.$fetchshit4['yea'].' </b>guilds </center></td></tr>'; 
 echo '</table>'; ?>
 <table>
 
