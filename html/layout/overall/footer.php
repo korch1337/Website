b8733f7640b6c683656f9e0d089320bd29866076
korch1337 <felixkorch1@gmail.com>
2015-02-01 19:36:38 +0100

    Update footer.php

--- html/layout/overall/footer.php
+++ html/layout/overall/footer.php
@@ -5,126 +5,14 @@
  
  <li class="homeButton"><a href="#"></a></li>
  
- <li><a href="#">Account</a>
- <div class="one_column_layout">
- <div class="col_1">
- <a class="headerLinks">Stream</a>
- 
- <a class="headerLinks">Messages</a>
- <a class="listLinks" style="font-weight:bold;color:white;">Inbox (7)</a>
- <a class="listLinks">Sent</a>
- <a class="listLinks">Trash</a>
- <a class="listLinks">Compose</a>
- 
- <a class="headerLinks">Pictures</a>
- <a class="listLinks">Manage Albums</a>
- <a class="listLinks">Manage Photos</a>
- <a class="listLinks">View All</a> 
- 
- <a class="headerLinks">Information</a>
- <a class="listLinks">Basic Info</a>
- <a class="listLinks">Contact</a>
- <a class="listLinks">Education</a>
- <a class="listLinks">Employment</a>
- <a class="listLinks">Links</a>
- 
- <a class="headerLinks">Friends</a>
- <a class="listLinks">All</a>
- <a class="listLinks" style="font-weight:bold;color:white;">Requested (13)</a>
- <a class="listLinks">Pending</a>
- </div>
- </div>
- </li> 
- 
- <!-- Begin 3 Columns Menu Item -->
- <li><a href="#">Profile</a>
- <div class="three_column_layout">
- 
- <div class="col_3">
- <h2>Example of Three Columns</h2>
- </div>
- 
- <div class="col_1">
- <p>This is a three columns example. Praesent gravida venenatis felis sed egestas.</p> 
- </div>
- <div class="col_1">
- <p>This is a bold text. Sed ac tortor lobortis sem gravida consequat et vel mi.</p> 
- </div>
- <div class="col_1">
- <p>This is an italic text sample. Curabitur arcu orci, iaculis vel bibendum gravida tortor lobortis.</p> 
- </div>
- 
- <div class="clear"></div> <!-- Use this clear div to separate rows of content -->
- 
- <div class="col_1">
- <p class="black_box">This is the black box styling. I use it for my private messenger and also my group chat feature.</p>
- </div>
- <div class="col_2">
- <p>Sed lacus est, iaculis sed sagittis ac, malesuada a felis. Vestibulum vitae dictum mauris. Aenean felis nisl, pulvinar quis blandit et, fermentum ut tortor. In condimentum nisi ut leo fermentum ornare. Praesent gravida venenatis felis sed egestas.</p>
- </div>
- 
- <div class="clear"></div> <!-- Use this clear div to separate rows of content -->
- 
- <div class="col_3">
- <h2>My Profile</h2>
- </div>
- 
- <div class="col_3"> <!-- Examples of paragraphs with side images -->
- <p>
- <img src="http://buckysroom.com/photos/users/815/resized/4e5320a6a11bfc8e1cdb23526a72af7d.jpg" class="img_left whiteBorder" alt="" />
- <a style="display:inline;font-weight:bold;color:white;">Bucky Roberts</a>
- <a href="#">Read more...</a>
- </p>
- </div>
- 
- </div>
- </li>
- <!-- End 3 Columns Menu Item -->
- 
- <li><a href="#">Two Columns</a><!-- Begin 2 Columns Menu Item -->
- <div class="two_column_layout"><!-- Begin 2 columns container -->
- <div class="col_2">
- <h2>Two Column Layout</h2>
- </div>
- <div class="col_1">
- <p>This is a two columns example. Praesent gravida venenatis felis sed egestas.</p> 
- </div>
- <div class="col_1">
- <p>Duis sit amet erat enim. Sed ac tortor lobortis sem gravida consequat et vel mi venenatis.</p> 
- </div>
- <div class="clear"></div>
- <div class="col_2">
- <p>Vivamus ut urna magna. Aenean vehicula feugiat leo, sit amet facilisis felis commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet erat enim.</p> 
- </div>
- </div><!-- End 2 columns container -->
- </li><!-- End 2 Columns Menu Item -->
- 
- <li>
- <a href="#">Programs</a> 
- <ul class="dropup">
- <li><a href="#">Calculator</a></li>
- <li><a href="#">Group Chat</a></li>
- <li><a href="#">Image Editor 2.0</a></li>
- <li><a href="#">Meme Generator</a></li>
- <li><a href="#">Private Messenger</a></li>
- <li><a href="#">Text Editor</a></li>
- </ul>
- </li> 
- 
-  <li>
- <a href="#">Bajs!!</a> 
- <ul class="dropup">
- <li><a href="#">asdasdasdasd</a></li>
- <li><a href="#">patrik luktar ko</a></li>
- <li><a href="#">davod '</a></li>
- <li><a href="#">bajsnss</a></li>
- <li><a href="#">Private Merrnger</a></li>
- <li><a href="#">Textrövor</a></li>
- </ul>
- </li> 
- 
- <li class="right"><a href="#" class="drop">Log Out</a>
- </li>
+ <?php
+						if (user_logged_in() === true) {
+							include 'layout/widgets/loggedin.php'; 
+						} else {
+							include 'layout/widgets/login.php'; 
+						}
+						if (user_logged_in() && is_admin($user_data)) include 'layout/widgets/Wadmin.php'; 
+					?>
  
  </ul><!-- End Footer Menu -->
  
