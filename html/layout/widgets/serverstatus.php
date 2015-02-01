faec00cd14438bc353d82d99e9be6f9e44f1fd2f
korch1337 <felixkorch1@gmail.com>
2015-02-01 06:39:59 +0100

    Update serverstatus.php

--- html/layout/widgets/serverstatus.php
+++ html/layout/widgets/serverstatus.php
@@ -1,1 +1,7 @@
-aaa
+<div class="sidebar">
+        <!-- insert your sidebar items here -->
+        <h3>Server Status</h3>
+        <h4>Online or Offline?</h4>
+        <h5>1st November 2013</h5>
+        <p>Is it online or offline???<br /><a href="#">Read more</a></p>
+        </div>
