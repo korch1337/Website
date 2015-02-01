5e51a317f92c3f6db2569f49f14943283ca32816
korch1337 <felixkorch1@gmail.com>
2015-02-01 18:46:26 +0100

    Update serverstatus.php

--- html/layout/widgets/serverstatus.php
+++ html/layout/widgets/serverstatus.php
@@ -1,7 +1,26 @@
 <div class="sidebar">
-        <!-- insert your sidebar items here -->
-        <h3>Server Status</h3>
-        <h4>Online or Offline?</h4>
-        <h5>1st November 2013</h5>
-        <p>Is it online or offline???<br /><a href="#">Read more</a></p>
+        <div class="m_online" align="center">
+					<b>Status:  
+						<?php
+							$status = true;
+							if ($config['status']['status_check']) {
+								@$sock = fsockopen ($config['status']['status_ip'], $config['status']['status_port'], $errno, $errstr, 1);
+								if(!$sock) {
+									echo '<font color="#9873DA"> Offline</font>';
+									$status = false;
+								}
+								else {
+									$info = chr(6).chr(0).chr(255).chr(255).'info';
+									fwrite($sock, $info);
+									$data='';
+									while (!feof($sock))$data .= fgets($sock, 1024);
+									fclose($sock);
+									echo '<font color="#9873DA"> Online</font><br><a href="whoisonline.php">';
+									echo user_count_online();
+									echo ' players online<br/></a>';
+								}
+							}
+						?></b>
+					<br>
+				</div>
         </div>
