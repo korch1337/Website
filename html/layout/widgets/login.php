7e4c5e7222176581a1b066e3cb496a06d95e0c1b
korch1337 <felixkorch1@gmail.com>
2015-02-01 19:39:06 +0100

    Update login.php

--- html/layout/widgets/login.php
+++ html/layout/widgets/login.php
@@ -1,1 +1,17 @@
-asd
+  <li><a href="#">Two Columns</a><!-- Begin 2 Columns Menu Item -->
+ <div class="two_column_layout"><!-- Begin 2 columns container -->
+ <div class="col_2">
+ <h2>Two Column Layout</h2>
+ </div>
+ <div class="col_1">
+ <p>This is a two columns example. Praesent gravida venenatis felis sed egestas.</p> 
+ </div>
+ <div class="col_1">
+ <p>Duis sit amet erat enim. Sed ac tortor lobortis sem gravida consequat et vel mi venenatis.</p> 
+ </div>
+ <div class="clear"></div>
+ <div class="col_2">
+ <p>Vivamus ut urna magna. Aenean vehicula feugiat leo, sit amet facilisis felis commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet erat enim.</p> 
+ </div>
+ </div><!-- End 2 columns container -->
+ </li>
