13f1db2f3ca3db7c9d4e823424dde5fdb4971225
korch1337 <felixkorch1@gmail.com>
2015-02-01 19:41:22 +0100

    Update login.php

--- html/layout/widgets/login.php
+++ html/layout/widgets/login.php
@@ -1,13 +1,23 @@
-  <li><a href="#">Two Columns</a><!-- Begin 2 Columns Menu Item -->
+  <li><a href="#">LOGIN HERE!</a><!-- Begin 2 Columns Menu Item -->
  <div class="two_column_layout"><!-- Begin 2 columns container -->
  <div class="col_2">
- <h2>Two Column Layout</h2>
+ <h2>Login form: </h2>
  </div>
  <div class="col_1">
- <p>This is a two columns example. Praesent gravida venenatis felis sed egestas.</p> 
+ <p>Enter your account number and password to the right!</p> 
  </div>
  <div class="col_1">
- <p>Duis sit amet erat enim. Sed ac tortor lobortis sem gravida consequat et vel mi venenatis.</p> 
+ <form action="login.php" method="post">
+				&nbsp;<i style="font-size:11px">Account number:</i> <br>
+			<input type="text" name="username">
+				&nbsp;<i style="font-size:11px">Password:</i> <br>
+			<input type="password" name="password">
+			<br><br><center><input type="submit" value="Log in"></center>
+			<?php
+				/* Form file */
+				Token::create();
+			?>
+		</form>
  </div>
  <div class="clear"></div>
  <div class="col_2">
