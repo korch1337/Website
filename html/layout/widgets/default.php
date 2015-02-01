04e37fd7175f6dcefc498d60f39bd92b8aebc4d5
korch1337 <felixkorch1@gmail.com>
2015-02-01 15:01:05 +0100

    Update default.php

--- html/layout/widgets/default.php
+++ html/layout/widgets/default.php
@@ -1,7 +1,6 @@
 <div class="sidebar">
         <!-- insert your sidebar items here -->
-        <h3>Latest News</h3>
-        <h4>What's the News?</h4>
-        <h5>1st November 2013</h5>
+        <h3>Character Search</h3>
+        <h4>Enter name:</h4>
         <p>Put your latest news item here, or anything else you would like in the sidebar!<br /><a href="#">Read more</a></p>
         </div>
