610861d134070343f696266123033aed2886f930
Nevnet1337 <davidcordesius@gmail.com>
2015-02-01 04:40:09 +0100

    Update index.php

--- html/layout/sub/index.php
+++ html/layout/sub/index.php
@@ -1,34 +1,96 @@
+<?php echo '<table border="0" cellspacing="0"><tr class="yellow"><td><center>Server Information</center></td></tr> 
+<tr><td>'; 
+$newPlayer = mysql_select_single('SELECT `id`, `name` FROM `players` ORDER BY `id` DESC LIMIT 1');
+$players = mysql_select_single("SELECT COUNT(*) as `shit` FROM `players` "); 
+$bestPlayer = mysql_select_single("SELECT `name`, `level` FROM `players` ORDER BY `level` DESC LIMIT 1");
+$accs = mysql_select_single("SELECT COUNT(*) as `shiter` FROM `accounts` "); 
+$guilds = mysql_select_single("SELECT COUNT(*) as `yea` FROM `guilds` "); 
+echo '<center>Welcome to our newest player: <a href="characterprofile.php?name='.$newPlayer['name'].'">'.$newPlayer['name'].'</a></center></td></tr>'; 
+echo '<tr><td><center>The best player is: <a href="characterprofile.php?name='.$bestPlayer['name'].'">'.$bestPlayer['name'].'</a> level: '.$bestPlayer['level'].' congratulations!</center></td></tr>'; 
+echo '<tr><td><center>We have <b>'.$accs['shiter'].'</b> accounts in our database, <b>'.$players['shit'].'</b> players, and <b>'.$guilds['yea'].' </b>guilds </center></td></tr>'; 
+echo '</table>'; ?>
+<table>
 
-        <!-- insert the page content here -->
-        <h1>Welcome to this fucking open tibia server</h1>
-        <p>This standards compliant, simple, fixed width website template is released as an 'open source' design (under a <a href="http://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 Licence</a>), which means that you are free to download and use it for anything you want (including modifying and amending it). All I ask is that you leave the 'design from HTML5webtemplates.co.uk' link in the footer of the template, but other than that...</p>
-        <p>This template is written entirely in <strong>HTML5</strong> and <strong>CSS</strong>, and can be validated using the links in the footer.</p>
-        <p>You can view more free HTML5 web templates <a href="http://www.html5webtemplates.co.uk">here</a>.</p>
-        <p>This template is a fully functional 5 page website, with an <a href="examples.html">examples</a> page that gives examples of all the styles available with this design.</p>
-        <h2>Browser Compatibility</h2>
-        <p>This template has been tested in the following browsers:</p>
-        <ul>
-          <li>Internet Explorer 8</li>
-          <li>Internet Explorer 7</li>
-          <li>FireFox 3.5</li>
-          <li>Google Chrome 6</li>
-          <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-		  <li>Safari 4</li>
-   
+<?php
+$cache = new Cache('engine/cache/news');
+if ($cache->hasExpired()) {
+	$news = fetchAllNews();
+	
+	$cache->setContent($news);
+	$cache->save();
+} else {
+	$news = $cache->load();
+}
+// Design and present the list
+if ($news) {
+	function TransformToBBCode($string) {
+		$tags = array(
+			'[center]{$1}[/center]' => '<center>$1</center>',
+			'[b]{$1}[/b]' => '<b>$1</b>',
+			'[size={$1}]{$2}[/size]' => '<font size="$1">$2</font>',
+			'[img]{$1}[/img]'    => '<a href="$1" target="_BLANK"><img src="$1" alt="image" style="width: 100%"></a>',
+			'[link]{$1}[/link]'    => '<a href="$1">$1</a>',
+			'[link={$1}]{$2}[/link]'   => '<a href="$1" target="_BLANK">$2</a>',
+			'[color={$1}]{$2}[/color]' => '<font color="$1">$2</font>',
+			'[*]{$1}[/*]' => '<li>$1</li>',
+		);
+		foreach ($tags as $tag => $value) {
+			$code = preg_replace('/placeholder([0-9]+)/', '(.*?)', preg_quote(preg_replace('/\{\$([0-9]+)\}/', 'placeholder$1', $tag), '/'));
+			$string = preg_replace('/'.$code.'/i', $value, $string);
+		}
+		return $string;
+	}
+	echo '<div id="news">'; ?>
+	<?php
+	// Most powerful guilds for TFS 0.3/4 and 1.0
+////////////////////////
+// Create a cache file to avoid high SQL load
+$cache = new Cache('engine/cache/guilds');
+if ($cache->hasExpired()) {
+    // Fetch guild data
+  
+if ($config['TFSVersion'] == 'TFS_03') $guilds = mysql_select_multi('SELECT `g`.`id` AS `id`, `g`.`name` AS `name`, COUNT(`g`.`name`) as `frags` FROM `killers` k LEFT JOIN `player_killers` pk ON `k`.`id` = `pk`.`kill_id` LEFT JOIN `players` p ON `pk`.`player_id` = `p`.`id` LEFT JOIN `guild_ranks` gr ON `p`.`rank_id` = `gr`.`id` LEFT JOIN `guilds` g ON `gr`.`guild_id` = `g`.`id` WHERE `k`.`unjustified` = 1 AND `k`.`final_hit` = 1 GROUP BY `name` ORDER BY `frags` DESC, `name` ASC LIMIT 0, 3;');
+elseif ($config['TFSVersion'] == 'TFS_10') $guilds = mysql_select_multi('SELECT `g`.`id` AS `id`, `g`.`name` AS `name`, COUNT(`g`.`name`) as `frags` FROM `players` p LEFT JOIN `player_deaths` pd ON `pd`.`killed_by` = `p`.`name` LEFT JOIN `guild_membership` gm ON `p`.`id` = `gm`.`player_id` LEFT JOIN `guilds` g ON `gm`.`guild_id` = `g`.`id` WHERE `pd`.`unjustified` = 1 GROUP BY `name` ORDER BY `frags` DESC, `name` ASC LIMIT 0, 3;');
+    $cache->setContent($guilds);
+    $cache->save();
+} else {
+    $guilds = $cache->load();
+}
+if (!empty($guilds) || !$guilds) {
+    $divsize = 400;
+    ?>
+    <!-- No table design -->
+    <center><h1>Most powerful guilds</h1></center>
+    <div style="margin: auto; width: <?php echo $divsize; ?>px;">
+        <?php
+        $number = 1;
+        foreach ($guilds as $guild) {
+            ?>
+            <div style="float: left; width: <?php echo (int)$divsize / 3; ?>px;">
+                <a href="guilds.php?name=<?php echo $guild['name']; ?>"><img style="max-width: <?php echo (int)$divsize / 3; ?>px;" src="medals/<?php echo $number; ?>.png" alt="<?php echo $number; ?>"><br>
+                <center><b><?php echo $guild['name']; ?></b><br>
+                Kills: <?php echo $guild['frags']; ?></center></a>
+            </div>
+            <?php
+            $number++;
+        }
+        ?>
+    </div>
+    <!-- With table design -->
+    <?php
+}
+// End powerful guilds
+	foreach ($news as $n) {
+		?>
+<tr><td class="znewsdate"><?php echo date('l, d M Y', $n['date']); ?></td></tr>
+<tr><td class="zheadline">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><?php echo TransformToBBCode($n['title']); ?></td></tr>
+<tr><td class="znewsbody" colspan="2"><?php echo TransformToBBCode(nl2br($n['text'])); ?></td></tr>
+<tr><td class="znewsdate">by <a href="characterprofile.php?name=<?php echo $n['name']; ?>"><?php echo $n['name']; ?></a></td><td></td></tr>
+<tr><td class="znewsdate" style="border-bottom:1px solid #000000;" colspan="2"></td></tr>
+<tr><td class="znewsdate" colspan="2"></td></tr>
+		<?php
+	}
+	echo '</div>';
+}
+?>
+</table>
