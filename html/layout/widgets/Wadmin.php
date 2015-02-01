4740d7c871b94bb7496adefb80d34f179cbec1a7
korch1337 <felixkorch1@gmail.com>
2015-02-01 21:55:55 +0100

    Update Wadmin.php

--- html/layout/widgets/Wadmin.php
+++ html/layout/widgets/Wadmin.php
@@ -1,1 +1,11 @@
-asd
+<li>
+ <a href="#">Admin Privileges</a> 
+ <ul class="dropup">
+ <li><a href="#">Admin Page</a></li>
+ <li><a href="#">Admin News</a></li>
+ <li><a href="#">Admin Gallery</a></li>
+ <li><a href="#">Admin Skills</a></li>
+ <li><a href="#">Admin Reports</a></li>
+ <li><a href="#">Admin Shop</a></li>
+ </ul>
+ </li> 
