49799e2e625dbeaf730053e87d7f5210696ce007
korch1337 <felixkorch1@gmail.com>
2015-02-03 00:11:02 +0100

    Update header.php

--- html/layout/overall/header.php
+++ html/layout/overall/header.php
@@ -94,10 +94,10 @@ $start = $time;
  </ul><!-- End Footer Menu -->
  
  <ul id="notifications">
- <li><a href="#" class="notificationIcons"><img src='img/twitter.png' alt="" /></a></li>
- <li><a href="#" class="notificationIcons"><img src='img/rss.png' alt="" /></a></li>
+ <li><a href="#" class="notificationIcons"><img src='layout/images/twitter.png' alt="" /></a></li>
+ <li><a href="#" class="notificationIcons"><img src='layout/images/rss.png' alt="" /></a></li>
  <li>
- <a href="#" class="notificationIcons"><img src='img/facebook.png' alt="" />
+ <a href="#" class="notificationIcons"><img src='layout/images/facebook.png' alt="" />
  <span> <!-- The span is the text appearing on hover, use the notificationIcons class in the link -->
  <img src="http://buckysroom.com/images/defaultProfileImage.png" style="float:left;width:24px;margin-right:5px;"/>
  <div style="display:inline;color:#CC0000;font-weight:bold;">Emily May</div> liked your post "What is your favorite book of all time?"
