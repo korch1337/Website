230413ae52fa396c80fd1732dad27db0bb152196
korch1337 <felixkorch1@gmail.com>
2015-02-02 15:55:52 +0100

    Update default.php

--- html/layout/widgets/default.php
+++ html/layout/widgets/default.php
@@ -1,7 +1,6 @@
 <img class="paperclip" src="layout/images/paperclip.png" alt="paperclip" />
         <!-- insert your sidebar items here -->
         <h3>Character Search</h3>
-        <div class="inner">
 		<li><font size="3">Character Name:<br></font>
 		<form type="submit" action="characterprofile.php" method="get">
 			<li><input type="text" name="name" class="search"><br><br>
