f426cb0e5e1b0ccc60e261f2858ff442b12e6f1a
korch1337 <felixkorch1@gmail.com>
2015-02-03 00:04:42 +0100

    Update login.php

--- html/layout/widgets/login.php
+++ html/layout/widgets/login.php
@@ -8,9 +8,9 @@
  </div>
  <div class="col_1">
  <form action="login.php" method="post">
-				&nbsp;<i style="font-size:11px">Account number:</i> <br>
+				&nbsp;<i style="font-size:15px">Account number:</i> <br>
 			<input type="text" name="username">
-				&nbsp;<i style="font-size:11px">Password:</i> <br>
+				&nbsp;<i style="font-size:15px">Password:</i> <br>
 			<input type="password" name="password">
 			<br><br><center><input type="submit" value="Log in"></center>
 			<?php
@@ -18,10 +18,6 @@
 				Token::create();
 			?>
 		</form>
- </div>
- <div class="clear"></div>
- <div class="col_2">
- <p>Vivamus ut urna magna. Aenean vehicula feugiat leo, sit amet facilisis felis commodo a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet erat enim.</p> 
  </div>
  </div><!-- End 2 columns container -->
  </li>
