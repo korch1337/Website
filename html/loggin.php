684a2754edc429e9c6b3bc42c7361eb40a194bf7
korch1337 <felixkorch1@gmail.com>
2015-02-03 20:39:33 +0100

    Update loggin.php

--- html/loggin.php
+++ html/loggin.php
@@ -1,6 +1,16 @@
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
 
 <h1>Login Page</h1>
-<p>This is a blank sample page.</p>
+<form action="login.php" method="post">
+				&nbsp;<i style="font-size:11px">Account number:</i> <br>
+			<input type="text" name="username">
+				&nbsp;<i style="font-size:11px">Password:</i> <br>
+			<input type="password" name="password">
+			<br><br><center><input type="submit" value="Log in"></center>
+			<?php
+				/* Form file */
+				Token::create();
+			?>
+		</form>
 
 <?php include 'layout/overall/footer.php'; ?>
