c724fe0669b627c2e2cc75c10df2ca2f119f36c3
korch1337 <felixkorch1@gmail.com>
2015-02-03 21:00:59 +0100

    Update Wadmin.php

--- html/layout/widgets/Wadmin.php
+++ html/layout/widgets/Wadmin.php
@@ -1,7 +1,7 @@
 <li><a href="#">Admin Privileges</a>
 		  <ul>
 		  <li><a href="admin.php">Admin Page</a></li>
-		  <li><a href="admin_news.php"></a>Admin News</li>
+		  <li><a href="admin_news.php">Admin News</a></li>
 		  <li><a href="admin_gallery.php">Admin Gallery</a></li>
 		  <li><a href="admin_skills.php">Admin Skills</a></li>
 		  <li><a href="admin_reports.php">Admin Reports</a></li>
