29a57ebd074e069782ee3374eb2d7f671f7e3a14
korch1337 <felixkorch1@gmail.com>
2015-02-03 20:35:30 +0100

    Create loggin.php

---
+++ html/loggin.php
@@ -0,0 +1,6 @@
+<?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
+
+<h1>Login Page</h1>
+<p>This is a blank sample page.</p>
+
+<?php include 'layout/overall/footer.php'; ?>
