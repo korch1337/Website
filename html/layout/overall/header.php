d66800cf70d410442f98329cc0090ac3f5035004
korch1337 <felixkorch1@gmail.com>
2015-02-03 18:37:59 +0100

    Update header.php

--- html/layout/overall/header.php
+++ html/layout/overall/header.php
@@ -75,6 +75,13 @@ $start = $time;
 		
 		
       </div>
+      
+      <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="layout/overall/jquery-scrolltofixed.js"></script>
+    <script type="text/javascript">
+        $('#menubar').scrollToFixed();
+    </script>
+      
     </div>
     
     <div id="site_content">
