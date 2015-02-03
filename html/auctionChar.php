02f42fb72ca5ba1369e69f6211ba467ae89cacf8
korch1337 <felixkorch1@gmail.com>
2015-02-03 22:26:16 +0100

    Update auctionChar.php

--- html/auctionChar.php
+++ html/auctionChar.php
@@ -19,7 +19,7 @@ if ($auction['characterAuction']) {
 	<?php echo '<table border="0" cellspacing="0"><tr class="yellow"><td><center>Player Auctions</center></td></tr> 
 <tr><td>'; 
 $aucPlayer = mysql_select_single('SELECT `id` FROM `znote_auction_player` ORDER BY `id` DESC');
-$aucName = mysql_select_single('SELECT `name` FROM `players` WHERE id=$aucPlayer ORDER BY `id` DESC');
+$aucName = mysql_select_single('SELECT `name` FROM `players` WHERE id='$aucPlayer' ORDER BY `id` DESC');
 echo '<a href="characterprofile.php?name='.$aucName['name'].'">'.$aucName['name'].'</a></td></tr>';
 echo '</table>'; ?>
 </table>
