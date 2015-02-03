c8e0345511c5b3a031896039f6480d459aca8f28
korch1337 <felixkorch1@gmail.com>
2015-02-03 20:59:52 +0100

    Update loggedin.php

--- html/layout/widgets/loggedin.php
+++ html/layout/widgets/loggedin.php
@@ -1,7 +1,7 @@
 <li><a href="#">My Account</a>
 		  <ul>
 		  <li><a href="myaccount.php">Account Page</a></li>
-		  <li><a href="createcharacter.php"></a>Create Character</li>
+		  <li><a href="createcharacter.php">Create Character</a></li>
 		  <li><a href="settings.php">Settings</a></li>
 		  <li><a href="logout.php">Log out</a></li>
 		  </ul>
