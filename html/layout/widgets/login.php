571e1101db1a01ec39aa636dc803f8228e67ebfc
korch1337 <felixkorch1@gmail.com>
2015-02-03 20:31:29 +0100

    Update login.php

--- html/layout/widgets/login.php
+++ html/layout/widgets/login.php
@@ -1,23 +1,6 @@
-  <li><a href="#">LOGIN HERE!</a><!-- Begin 2 Columns Menu Item -->
- <div class="two_column_layout"><!-- Begin 2 columns container -->
- <div class="col_2">
- <h2>Login form: </h2>
- </div>
- <div class="col_1">
- <p>Enter your account number and password to the right!</p> 
- </div>
- <div class="col_1">
- <form action="login.php" method="post">
-				&nbsp;<i style="font-size:15px">Account number:</i> <br>
-			<input type="text" name="username">
-				&nbsp;<i style="font-size:15px">Password:</i> <br>
-			<input type="password" name="password">
-			<br><br><center><input type="submit" value="Log in"></center>
-			<?php
-				/* Form file */
-				Token::create();
-			?>
-		</form>
- </div>
- </div><!-- End 2 columns container -->
- </li>
+<li><a href="index.php">Account</a>
+		  <ul>
+		  <li><a href="register.php">Create Account</a></li>
+		  <li><a href="recovery.php">Lost Account</a></li>
+		  </ul>
+		  </li>
