2a479c1a8222ca0084269b42bb6fa4acb0ec8a39
korch1337 <felixkorch1@gmail.com>
2015-02-03 20:36:23 +0100

    Update login.php

--- html/layout/widgets/login.php
+++ html/layout/widgets/login.php
@@ -1,5 +1,6 @@
 <li><a href="index.php">Account</a>
 		  <ul>
+		  <li><a href="loggin.php">Log in</a></li>
 		  <li><a href="register.php">Create Account</a></li>
 		  <li><a href="recovery.php">Lost Account</a></li>
 		  </ul>
