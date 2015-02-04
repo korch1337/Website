df365a1c203f7b3451b711c92e437de6eca362f7
korch1337 <felixkorch1@gmail.com>
2015-02-05 00:51:23 +0100

    Update sellcharacter.php

--- html/sellcharacter.php
+++ html/sellcharacter.php
@@ -1,12 +1,17 @@
 
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
 
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['quantity']) {
+if (is_int($_POST['quantity']) { $quantity= $_POST['quantity']; }
+
+echo $quantity;
+?>
+
 <h1>Sell your character :DDD</h1>
 <p>Enter your price: <form method='post' action=''>
 <input name='quantity' type='text' id='quantity' size='3' maxlength='3' />
 <input type='submit'  name='submit' value='submit' />
 </form></p>
-<?php
-echo $quantity;
-?>
+
 <?php include 'layout/overall/footer.php'; ?>
