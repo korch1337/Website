613b43162409ab17063db07b322e07b149935c6c
korch1337 <felixkorch1@gmail.com>
2015-02-05 00:48:50 +0100

    Update sellcharacter.php

--- html/sellcharacter.php
+++ html/sellcharacter.php
@@ -2,10 +2,9 @@
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
 
 <h1>Sell your character :DDD</h1>
-<p>Enter your price: </p>
-<form method='post' action=''>
+<p>Enter your price: <form method='post' action=''>
 <input name='quantity' type='text' id='quantity' size='3' maxlength='3' />
 <input type='submit'  name='submit' value='submit' />
-</form>
+</form></p>
 
 <?php include 'layout/overall/footer.php'; ?>
