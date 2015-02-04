81066366c03f782ef6e233c9a2f50ba39d388843
korch1337 <felixkorch1@gmail.com>
2015-02-04 18:07:38 +0100

    Update auctionChar.php

--- html/auctionChar.php
+++ html/auctionChar.php
@@ -14,7 +14,7 @@ if ($auction['characterAuction']) {
 	if($result->num_rows){
 		$rows = $result->fetch_assoc();
 		
-		echo '<pre>', print_r($rows), '</pre>';
+		echo '<pre>', print($rows), '</pre>';
 	}
 	
 	?>
