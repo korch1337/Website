1395951691ce71e28e9f4b8c30613b3e45b8ae11
korch1337 <felixkorch1@gmail.com>
2015-02-04 18:23:17 +0100

    Update auctionChar.php

--- html/auctionChar.php
+++ html/auctionChar.php
@@ -12,7 +12,7 @@ if ($auction['characterAuction']) {
 	$result = $db->query("SELECT a.name FROM players AS a, znote_auction_player AS b WHERE a.id=b.player_id");
 	
 	if($result->num_rows){
-		$rows = $result->fetch_assoc();
+		$rows = $result->fetch_all(MYSQLI_ASSOC);
 		foreach($rows as $row){
 			echo '<p>', $row['name'], '<br>', '</p>';
 		}
