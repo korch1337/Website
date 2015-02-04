bad897a916bbe47b1c2030ee0eb7e92de6e4035b
korch1337 <felixkorch1@gmail.com>
2015-02-04 23:03:05 +0100

    Update auctionChar.php

--- html/auctionChar.php
+++ html/auctionChar.php
@@ -24,7 +24,7 @@ if ($auction['characterAuction']) {
 	</tr>
 	<tr>
 	       <?php while($row = $aucPlayers->fetch_object()){
-			echo nl2br("<td> $row->name </td> <td> $row->level </td> <td> $row->vocation </td> <td> Image here </td> <td> $row->price </td>" . "\n");
+			echo '$row->name';
 	       }
 		   $aucPlayers->free();
 	       ?>
