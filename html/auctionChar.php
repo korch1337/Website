92e5d2f0e639db2bdb86804331a6d0c60a4a1cf9
korch1337 <felixkorch1@gmail.com>
2015-02-04 17:56:01 +0100

    Update auctionChar.php

--- html/auctionChar.php
+++ html/auctionChar.php
@@ -12,7 +12,9 @@ if ($auction['characterAuction']) {
 	$result = $db->query("SELECT name FROM players");
 	
 	if($result->num_rows){
-		echo 'It worked, im best';
+		$rows = $result->fetch_assoc();
+		
+		print_r($rows);
 	}
 	
 	?>
