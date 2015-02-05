d3a53538bc1da315e314a54bf314d47fd0a4e11c
korch1337 <felixkorch1@gmail.com>
2015-02-05 23:45:16 +0100

    Update sellcharacter.php

--- html/sellcharacter.php
+++ html/sellcharacter.php
@@ -5,19 +5,7 @@ $accid = $user_data['id'];
 
 $getCharacters = $db->query("SELECT name FROM players WHERE account_id=$accid"); 
 $characters = $getCharacters->fetch_object();
+echo '<h1>'.'Sell Character'.'</h1>';
 ?>
-
-<h1>Sell Character</h1>
-<ul>
-<li>
-
-Choose Character:<br>
-				<select name="selected_character">
-				
-				<option value="<?php echo '1'; ?>"><?php echo $chars->name ?></option>
-				</select>				
-				
-</li>
-</ul>
 
 <?php include 'layout/overall/footer.php'; ?>
