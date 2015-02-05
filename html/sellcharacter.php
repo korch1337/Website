9aaf3bda5604f92d746d15af30e3fb135cf42358
korch1337 <felixkorch1@gmail.com>
2015-02-05 22:16:19 +0100

    Update sellcharacter.php

--- html/sellcharacter.php
+++ html/sellcharacter.php
@@ -1,6 +1,15 @@
-<div class="messagepop pop">
-    <form method="post" id="new_message" action="/messages">
-        <p><label for="price">Enter your price: </label><input type="text" size="30" name="price" id="price" /></p>
-        <p><input type="submit" value="Confirm" name="commit" id="message_submit"/> or <a class="close" href="/">Cancel</a></p>
-    </form>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script>
+  $(function() {
+    $( "#dialog" ).dialog();
+  });
+  </script>
+</head>
+<body>
+ 
+<div id="dialog" title="Basic dialog">
+  <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
 </div>
