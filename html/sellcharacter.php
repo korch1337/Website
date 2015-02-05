91c0ac9b686c2c9abd588ed929742fc01263e142
korch1337 <felixkorch1@gmail.com>
2015-02-05 01:04:47 +0100

    Update sellcharacter.php

--- html/sellcharacter.php
+++ html/sellcharacter.php
@@ -1,21 +1,7 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['quantity']) {
-if (is_int($_POST['quantity']) { $quantity= $_POST['quantity']; }
+<?php require 'connect.php'; ?>
 
-
-?>
-<?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
-
-
-
-<h1>Sell your character :DDD</h1>
-<p>Enter your price: <form method='post' action=''>
-<input name='quantity' type='text' id='quantity' size='3' maxlength='3' />
-<input type='submit'  name='submit' value='submit' />
-</form></p>
-
-<?php
-echo $quantity;
-?>
-
-<?php include 'layout/overall/footer.php'; ?>
+<?php $sellCharacter = $db->query("
+INSERT INTO znote_auction_player
+VALUES(4, 3, , , , , , 200)
+"); 
+	?>
