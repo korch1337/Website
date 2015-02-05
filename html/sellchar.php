773f0fb5980b343d13072254181729e98c0a08ab
korch1337 <felixkorch1@gmail.com>
2015-02-05 13:18:49 +0100

    Update sellchar.php

--- html/sellchar.php
+++ html/sellchar.php
@@ -3,7 +3,7 @@
 $bajs = $_GET['id'];
  $aucPlayers = $db->query("SELECT a.name, a.id, a.account_id, a.vocation, a.level FROM players AS a WHERE a.id=$bajs"); 
    
-   $row = $aucPlayers->fetch_object()
+   $row = $aucPlayers->fetch_object();
    echo 'DO YOU REALLY WANT TO SELL THIS CHARACTER!??!';
    echo 'Name: ', $row->name,'player id: ', $row->id,'Account ID: ', $row->account_id, 'Vocation: ', $row->vocation, 'Level: ', $row->level;
    $aucPlayers->free();
