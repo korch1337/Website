a1c7e974610aec55536b3e37fe7b864e7b06be7e
korch1337 <felixkorch1@gmail.com>
2015-02-05 23:10:50 +0100

    Update sellcharacter.php

--- html/sellcharacter.php
+++ html/sellcharacter.php
@@ -2,6 +2,14 @@
 
 <h1>Sell Character</h1>
 
-
+<li>
+				
+				Choose Character:<br>
+				<select name="selected_character">
+				<?php foreach ($available_chars as $chars) { ?>
+				<option value="<?php echo $id; ?>"><?php echo vocation_id_to_name($id); ?></option>
+				<?php } ?>
+				</select>
+			</li>
 
 <?php include 'layout/overall/footer.php'; ?>
