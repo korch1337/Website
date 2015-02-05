e476da0dd3d505758fc3673425a14fa6961330ee
korch1337 <felixkorch1@gmail.com>
2015-02-05 11:10:00 +0100

    Update sellcharacter.php

--- html/sellcharacter.php
+++ html/sellcharacter.php
@@ -1,28 +1,27 @@
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; require 'connect.php'; ?>
 
-<!-- <h1>Sell Your Character</h1>
+<h1>Sell Your Character</h1>
 <p>Enter char id: </p>
-<form action="sellcharacter.php" method="post">
+<form action=sell() method="post">
 ID: <input type="text" name="id">
 <input type="submit" name="submit" value="Auction">
-</form> -->
-<script>
-console.log(<?php
+</form>
 
-// function sell(){
+<?php
+
+function sell(){
   $aucPlayers = $db->query("SELECT name, id from players WHERE id=1"); 
    while($row = $aucPlayers->fetch_object()){
 			echo $row->name;
 	       }
 		   $aucPlayers->free();
-//}
+}
 
 //if(isset($_POST['submit']))
 //{
  //  sell();
 //} 
 
-?>)
+?>
 
-</script>
 <?php include 'layout/overall/footer.php'; ?>
