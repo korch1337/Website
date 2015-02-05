3396d66778bc12b85365a8782480be782ea7b68f
korch1337 <felixkorch1@gmail.com>
2015-02-05 22:46:36 +0100

    Update myaccount.php

--- html/myaccount.php
+++ html/myaccount.php
@@ -3,6 +3,21 @@
   <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
   <link rel="stylesheet" href="/resources/demos/style.css">
 <?php require_once 'engine/init.php';
+if (isset($_POST)) {
+  <script>
+  $(function() {
+    $( "#dialog" ).dialog();
+  });
+  </script>
+  
+  <div id="dialog" title="Confirmation and price">
+  <p>Enter char id: </p>
+<form type="submit" action="sellchar.php" method="get">
+ID: <input type="text" name="id">
+<input type="submit" value="Auction">
+</form>
+</div>
+}
 protect_page();
 include 'layout/overall/header.php'; 
 #region CANCEL CHARACTER DELETE
@@ -245,7 +260,7 @@ if ($render_page) {
 				foreach ($char_array as $value) {
 					// characters: [0] = name, [1] = level, [2] = vocation, [3] = town_id, [4] = lastlogin, [5] = online
 					echo '<tr>';
-					echo '<td><a href="characterprofile.php?name='. $value['name'] .'">'. $value['name'] .'</a></td><td>'. $value['level'] .'</td><td>'. $value['vocation'] .'</td><td>'. $value['town_id'] .'</td><td>'. $value['lastlogin'] .'</td><td>'. $value['online'] .'</td><td>'. hide_char_to_name(user_character_hide($value['name'])) .'</td><td>'.'<form action="sellcharacter.php" method="post"><input type="submit" value="SELL"></form>'.'</td>';
+					echo '<td><a href="characterprofile.php?name='. $value['name'] .'">'. $value['name'] .'</a></td><td>'. $value['level'] .'</td><td>'. $value['vocation'] .'</td><td>'. $value['town_id'] .'</td><td>'. $value['lastlogin'] .'</td><td>'. $value['online'] .'</td><td>'. hide_char_to_name(user_character_hide($value['name'])) .'</td><td>'.'<form method="post"><input type="submit" value="SELL"></form>'.'</td>';
 					echo '</tr>';
 					$characters[] = $value['name'];
 				}
