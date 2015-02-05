e8daaec1fc7f43485a750a035550ac4bcda1a2ad
korch1337 <felixkorch1@gmail.com>
2015-02-05 22:59:22 +0100

    Update sellcharacter.php

--- html/sellcharacter.php
+++ html/sellcharacter.php
@@ -1,3 +1,7 @@
+<?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
+
+<h1>Sell Character</h1>
+
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
@@ -15,3 +19,5 @@ ID: <input type="text" name="id">
 <input type="submit" value="Auction">
 </form>
 </div>
+
+<?php include 'layout/overall/footer.php'; ?>
