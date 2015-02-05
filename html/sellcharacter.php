d1caa6c8d6ebe49526d3fd21905c7cf0e34c414a
korch1337 <felixkorch1@gmail.com>
2015-02-05 02:07:18 +0100

    Update sellcharacter.php

--- html/sellcharacter.php
+++ html/sellcharacter.php
@@ -10,7 +10,9 @@ ID: <input type="text" name="id">
 <?php
 
 function sell(){
-  echo 'hej';
+  $aucPlayers = $db->query("SELECT name from players WHERE id=1"); 
+  $rows = $aucPlayers->fetch_assoc();
+  echo '<pre>', print_r($rows), '</pre>';
 }
 
 if(isset($_POST['submit']))
