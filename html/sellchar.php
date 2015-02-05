fc85dd4e8b5664d1bc8a483eb4d9466ecce1ba6d
korch1337 <felixkorch1@gmail.com>
2015-02-05 13:00:46 +0100

    Create sellchar.php

---
+++ html/sellchar.php
@@ -0,0 +1,9 @@
+<?php require 'connect.php';
+
+  $aucPlayers = $db->query("SELECT name, id from players WHERE id=$_GET["id"]"); 
+   while($row = $aucPlayers->fetch_object()){
+			echo $row->name;
+	       }
+		   $aucPlayers->free();
+
+?>
