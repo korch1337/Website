f1770956b7e4656a854e1c120d07752e63f93945
Nevnet1337 <davidcordesius@gmail.com>
2015-02-05 23:28:30 +0100

    Update david.js

--- html/david.js
+++ html/david.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 var $menu = $('ul > li > ul > li');
 
 $menu.mouseenter(function() {
-$menu.fadeTo('fast', 0.8);
+$menu.fadeTo('fast', 0.2);
 });
 $menu.mouseleave(function() {
 $menu.fadeTo('fast', 1);
