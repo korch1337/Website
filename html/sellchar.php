555a4882463845d5d8ad2fccb54068d496642ece
korch1337 <felixkorch1@gmail.com>
2015-02-05 13:12:55 +0100

    Update sellchar.php

--- html/sellchar.php
+++ html/sellchar.php
@@ -1,9 +1,9 @@
 <?php require 'connect.php';
 
 $bajs = $_GET['id'];
- $aucPlayers = $db->query("SELECT name, id from players WHERE id=$bajs"); 
+ $aucPlayers = $db->query("SELECT a.id, a.account_id, a.vocation, a.level FROM players AS a WHERE a.id=$bajs"); 
    while($row = $aucPlayers->fetch_object()){
-			echo $row->name;
+			echo $row->name, $row->level;
 	       }
 		   $aucPlayers->free();
 
