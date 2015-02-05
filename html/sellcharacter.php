d922e0b406d056c9b3cd2dfd563c9a423ab8c67f
korch1337 <felixkorch1@gmail.com>
2015-02-05 22:18:58 +0100

    Update sellcharacter.php

--- html/sellcharacter.php
+++ html/sellcharacter.php
@@ -10,6 +10,10 @@
 </head>
 <body>
  
-<div id="dialog" title="Basic dialog">
-  <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
+<div id="dialog" title="Confirmation and price">
+  <p>Enter char id: </p>
+<form type="submit" action="sellchar.php" method="get">
+ID: <input type="text" name="id">
+<input type="submit" value="Auction">
+</form>
 </div>
