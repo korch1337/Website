d3fc2de8139224a3da42bff3447842f27de485c9
korch1337 <felixkorch1@gmail.com>
2015-02-05 13:04:57 +0100

    Update sellchar.php

--- html/sellchar.php
+++ html/sellchar.php
@@ -1,9 +1,7 @@
 <?php require 'connect.php';
 
-  $aucPlayers = $db->query("SELECT name, id from players WHERE id=$_GET['id']"); 
-   while($row = $aucPlayers->fetch_object()){
-			echo $row->name;
-	       }
-		   $aucPlayers->free();
+$bajs = $_GET['id'];
+echo $bajs;
+
 
 ?>
