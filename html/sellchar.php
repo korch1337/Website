eddb4787f35b2450f6d0cb0e642da9baf6bad476
korch1337 <felixkorch1@gmail.com>
2015-02-05 13:18:15 +0100

    Update sellchar.php

--- html/sellchar.php
+++ html/sellchar.php
@@ -1,11 +1,12 @@
 <?php require 'connect.php';
 
 $bajs = $_GET['id'];
- $aucPlayers = $db->query("SELECT a.id, a.account_id, a.vocation, a.level FROM players AS a WHERE a.id=$bajs"); 
-   while($row = $aucPlayers->fetch_object()){
-			echo $row->id, $row->account_id, $row->vocation, $row->level;
-	       }
-		   $aucPlayers->free();
+ $aucPlayers = $db->query("SELECT a.name, a.id, a.account_id, a.vocation, a.level FROM players AS a WHERE a.id=$bajs"); 
+   
+   $row = $aucPlayers->fetch_object()
+   echo 'DO YOU REALLY WANT TO SELL THIS CHARACTER!??!';
+   echo 'Name: ', $row->name,'player id: ', $row->id,'Account ID: ', $row->account_id, 'Vocation: ', $row->vocation, 'Level: ', $row->level;
+   $aucPlayers->free();
 
 
 ?>
