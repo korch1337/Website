de127a3a0848bb60026a0261637e6ff5beee1e4e
korch1337 <felixkorch1@gmail.com>
2015-02-06 02:15:38 +0100

    Update sellchar.php

--- html/sellchar.php
+++ html/sellchar.php
@@ -7,11 +7,11 @@ $name = $_GET['selected_character'];
  $aucPlayers = $db->query("SELECT a.name, a.id, a.account_id, a.vocation, a.level FROM players AS a WHERE a.name='$name'"); 
    
    $row = $aucPlayers->fetch_object();
-   echo 'YOUR CHARACTER HAS BEEN PUT ON THE MARKET.';
    $db->query("INSERT INTO znote_auction_player(`player_id`, `account_id`, `vocation`, `level`, `price`)
    VALUES ($row->id, $row->account_id, $row->vocation, $row->level, $price)");
    $aucPlayers->free();
-
+   
+   echo 'YOUR CHARACTER HAS BEEN PUT ON THE MARKET.';
 
 ?>
 
