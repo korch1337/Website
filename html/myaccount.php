4ee59664e295239e80b4fec9d228af5539e986cf
korch1337 <felixkorch1@gmail.com>
2015-02-06 03:13:14 +0100

    Update myaccount.php

--- html/myaccount.php
+++ html/myaccount.php
@@ -228,7 +228,7 @@ if ($render_page) {
 			You have <?php echo $user_data['premdays']; ?> days remaining premium account.</p>
 		<h2>Character List: <?php echo $char_count; ?> characters.</h2>
 		<div id="sell">
-		<a class="pure-button" href="sellcharacter.php">Sell a Character!</a>
+		<button class="pure-button">Sell A Character!</button>
 		</div>
 		<?php
 		// Echo character list!
