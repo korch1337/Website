eb47abeb7ddb3738bc80b6b0b37a996f65d8a825
korch1337 <felixkorch1@gmail.com>
2015-02-07 19:51:38 +0100

    Create buycharacter.php

---
+++ html/buycharacter.php
@@ -0,0 +1,6 @@
+<?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
+
+<h1>Confirm your purchase</h1>
+<p>This is a blank sample page.</p>
+
+<?php include 'layout/overall/footer.php'; ?>
