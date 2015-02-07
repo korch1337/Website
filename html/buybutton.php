124d089268004a01ec1c90ca3c9987698149820c
Nevnet1337 <davidcordesius@gmail.com>
2015-02-07 21:32:09 +0100

    Update buybutton.php

--- html/buybutton.php
+++ html/buybutton.php
@@ -4,13 +4,15 @@ var $hehe = $("#buy");
 
 $hehe.click(function(){
         
-var check = confirm("Are you sure?");
-if (check === true){
+var check = prompt("TYPE 1 IF U WANT TO BUY! 0 IF U DON'T!");
+if (check === 1){
         <?php 
         echo 'hello';
         ?>
         
-}
+}else{
+        confirm("You didn't buy the character!");
+};
 
 
 
