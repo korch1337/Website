edd901b92e743e695a1c86e8b0b79475af23d425
Nevnet1337 <davidcordesius@gmail.com>
2015-02-07 21:20:57 +0100

    Update buybutton.js

--- html/buybutton.js
+++ html/buybutton.js
@@ -3,23 +3,8 @@ $(document).ready(function(){
 var $hehe = $("#buy");
 
 $hehe.click(function(){
-$hehe.dialog({
-        resizable: false,
-        modal: true,
-        title: "Modal",
-        height: 250,
-        width: 400,
-        buttons: {
-            "Yes": function () {
-                $(this).dialog('close');
-                callback(true);
-            },
-                "No": function () {
-                $(this).dialog('close');
-                callback(false);
-            }
-        }
-    });
+        
+confirm("Are you sure?");
 
 
 
