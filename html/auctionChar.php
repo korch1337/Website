f0dff287de0b6db585b0526fed4d281562d1ac1b
korch1337 <felixkorch1@gmail.com>
2015-02-07 23:04:09 +0100

    Update auctionChar.php

--- html/auctionChar.php
+++ html/auctionChar.php
@@ -33,7 +33,7 @@ if ($auction['characterAuction']) {
 	       
 	       while($row = $aucPlayers->fetch_object()){
 			echo '<tr>', '<td>', '<a href="characterprofile.php?name='.$row->name.'">' ,$row->name, '</a>', '</td>', '<td>', $row->level, '</td>', '<td>', vocation_id_to_name($row->vocation), '</td>', '<td>','Image?', '</td>', '<td>', '<a href="buypoints.php">' ,$row->price, '</a>', '</td>';
-			echo '<td>','<button onclick="myFunction('.$row->id.')">','Buy this character','</button>','</td>','</tr>';
+			echo '<td>','<button onclick="myFunction('.$row->id.');">','Buy this character','</button>','</td>','</tr>';
 	       }
 		   $aucPlayers->free();
 	       ?>
