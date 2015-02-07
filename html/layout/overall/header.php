67bdde7acf154aba95c63a16a9cd8ebde8187c0a
Nevnet1337 <davidcordesius@gmail.com>
2015-02-07 21:15:08 +0100

    Update header.php

--- html/layout/overall/header.php
+++ html/layout/overall/header.php
@@ -17,6 +17,7 @@ $start = $time;
   <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz" />
   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
      <script type="text/javascript" src="david.js"></script>
   <link rel="stylesheet" type="text/css" href="layout/main.css" />
 </head>
