b831d2f0afba157fcc6a2723c710f4b3f3c06674
Nevnet1337 <davidcordesius@gmail.com>
2015-02-07 01:29:38 +0100

    Update davidajaxtest.php

--- html/davidajaxtest.php
+++ html/davidajaxtest.php
@@ -1,7 +1,59 @@
-Hello
+<div style="width:485px;height:975px;box-shadow:0 0 20px #000;font-family:Arial, sans-serif;border:2px solid black;margin:5px auto;text-align:center;overflow:hidden;position:relative;" id="templeAnimationWindow3"></div>
+
+<script type="text/javascript">
+var URL = ;
+var updateIntervalInSeconds = 1;
+
+function handler(data)
+    {
+        setTimeout(update, updateIntervalInSeconds * 1000);
+        //HTML SOM SKA ANVÄNDA KODEN SOM SKICKAS FRÅN SERVERN!
+        
+        
+        
+        document.getElementById('templeAnimationWindow3').innerHTML = function(){update()};
+    }
 
 
-
-
-var davidreq;
-var davidreq = new XMLHttprequest();
+  function update()
+    {
+        var xhr;
+        try
+        {
+            xhr=new XMLHttpRequest();
+        }
+        catch (e)
+        {
+            try
+            {
+                xhr=new ActiveXObject("Msxml2.XMLHTTP");
+            }
+            catch (e)
+            {
+                try
+                {
+                    xhr=new ActiveXObject("Microsoft.XMLHTTP");
+                }
+                catch (e)
+                {
+                    alert("Your browser does not support AJAX!");
+                    return false;
+                }
+            }
+        }
+        xhr.open("GET", URL + "?"+Math.random(), true);
+        xhr.onreadystatechange=function()
+        {
+            // status 4 = query realized without problems
+            if(xhr.readyState==4)
+            {
+                // if query failed for some reason, it will stop animation :(
+                handler(xhr.responseText);
+            }
+        }
+        xhr.send(null);
+    }
+    update();
+  
+  alertContents();
+</script>
