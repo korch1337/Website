20c8462e79ec9122b97b35363f2154dca8125c37
korch1337 <felixkorch1@gmail.com>
2015-02-09 00:35:39 +0100

    Update getLatestData.php

--- html/getLatestData.php
+++ html/getLatestData.php
@@ -1,13 +1,13 @@
 <?php 
 
 $json = file_get_contents("general.json");
-$bounty = json_decode($json, true);
+$bounties = json_decode($json, true);
 
 // Adding new data:
-$bounty[3] = array('name' => 'Foo', 'price' => 'Bar');
+$bounties[3] = array('name' => 'Foo', 'price' => 'Bar');
 
 // Writing modified data:
 $fp = fopen('general.json', 'w');
-fwrite($fp, json_encode($bounty));
+fwrite($fp, json_encode($bounties));
 fclose($fp);
 ?>
