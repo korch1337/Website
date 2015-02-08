259d4b58cf90403d901a8a62dcf68d9e4ba75891
Nevnet1337 <davidcordesius@gmail.com>
2015-02-08 02:32:46 +0100

    Update buycharacter.php

--- html/buycharacter.php
+++ html/buycharacter.php
@@ -1,4 +1,10 @@
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
+<script src="auctionChar.php" type="text/javascript"></script> 
+
+<script type="text/javascript">
+myfunction();
+
+</script>
 
 <h1>Confirm your purchase</h1>
 <p>This is a blank sample page.</p>
