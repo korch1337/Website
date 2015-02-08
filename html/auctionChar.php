f372ba7b886c860772f97105cc4b5136b428b064
korch1337 <felixkorch1@gmail.com>
2015-02-08 15:53:00 +0100

    Update auctionChar.php

--- html/auctionChar.php
+++ html/auctionChar.php
@@ -31,7 +31,7 @@ if ($auction['characterAuction']) {
 	
 	       <?php while($row = $aucPlayers->fetch_object()){
 			echo '<tr>', '<td>', '<a href="characterprofile.php?name='.$row->name.'">' ,$row->name, '</a>', '</td>', '<td>', $row->level, '</td>', '<td>', vocation_id_to_name($row->vocation), '</td>', '<td>','Image?', '</td>', '<td>', '<a href="buypoints.php">' ,$row->price, '</a>', '</td>';
-			echo '<td>','<form method="POST" onSubmit="'.return confirm('are you sure?').'" action="'.$_SERVER['PHP_SELF'].'">','<input type="submit" name="'.$row->id.'" value="Buy '.$row->name.'">','</form>','</td>','</tr>';
+			echo '<td>','<form method="POST" onSubmit="'.return confirm("are you sure?").'" action="'.$_SERVER['PHP_SELF'].'">','<input type="submit" name="'.$row->id.'" value="Buy '.$row->name.'">','</form>','</td>','</tr>';
 			
 			if(isset($_POST[$row->id])) {
 				
