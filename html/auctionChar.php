2768d67b96ec673c2d11f2255137c0a866fd578e
korch1337 <felixkorch1@gmail.com>
2015-02-08 15:21:16 +0100

    Update auctionChar.php

--- html/auctionChar.php
+++ html/auctionChar.php
@@ -16,7 +16,7 @@ if ($auction['characterAuction']) {
 <table class="auction_char">
 	
 	<?php $aucPlayers = $db->query("SELECT a.name,a.id, a.level, a.vocation, b.price FROM players AS a, znote_auction_player as b WHERE a.id=b.player_id ORDER BY a.level DESC"); 
-	
+	$acc_id = $user_data['id'];
 	?>
 	
 	
@@ -33,7 +33,7 @@ if ($auction['characterAuction']) {
 			echo '<td>','<form method="POST" action="'.$_SERVER['PHP_SELF'].'">','<input type="submit" name="'.$row->id.'" value="Buy '.$row->name.'">','</form>','</td>','</tr>';
 			
 			if(isset($_POST[$row->id])) {
-				$db->query("UPDATE players SET account_id=$user_data['id']; WHERE id=$row->id");
+				$db->query("UPDATE players SET account_id=$acc_id WHERE id=$row->id");
 				$db->query("DELETE FROM znote_auction_player WHERE player_id = $row->id");
 				echo '<script type="text/javascript">';
 				echo 'window.location.reload()';
