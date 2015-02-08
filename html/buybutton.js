72eeb5fd6c4c350f9c736ddf64e588c6d5e9721f
Nevnet1337 <davidcordesius@gmail.com>
2015-02-08 01:45:31 +0100

    Update and rename buybutton.php to buybutton.js

--- html/buybutton.js
+++ html/buybutton.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
 
-var $hehe = $("#buy");
+var test = 
 
+
+
+
+
+
+
+var $hehe = $("#buy");
 $hehe.click(function(){
         
 var check = prompt("TYPE 1 IF U WANT TO BUY! 0 IF U DON'T!");
 if (check == 1){
 
-        <?php 
-        echo 'hello';
-        ?>
         
 }else{
         confirm("You didn't buy the character!");
