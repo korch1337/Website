185fb323beb3ed5dab17f8d9399da91e376e9a2c
korch1337 <felixkorch1@gmail.com>
2015-02-08 01:30:18 +0100

    Update auctionChar.php

--- html/auctionChar.php
+++ html/auctionChar.php
@@ -28,18 +28,14 @@ if ($auction['characterAuction']) {
 	
 	       <?php while($row = $aucPlayers->fetch_object()){
 			echo '<tr>', '<td>', '<a href="characterprofile.php?name='.$row->name.'">' ,$row->name, '</a>', '</td>', '<td>', $row->level, '</td>', '<td>', vocation_id_to_name($row->vocation), '</td>', '<td>','Image?', '</td>', '<td>', '<a href="buypoints.php">' ,$row->price, '</a>', '</td>';
-			echo '<td>','<button onclick="myFunction('.$row->id.')">','Buy this character','</button>','</td>','</tr>';
+			echo '<td>','<form method="POST" action="">','<input type="SUBMIT" name="$row->id" value="Buy '.$row->name.'">','</form>','</td>','</tr>';
 	       }
 		   $aucPlayers->free();
+		   
+		   
 	       ?>
 </table>
 	<?php
 } else echo "<p>Character shop auctioning system is disabled.</p>";
 
 include 'layout/overall/footer.php'; ?>
-
-<script>
-function myFunction($id){ 
-	       <?php $db->query("UPDATE players SET account_id=1 WHERE id=$id"); ?>
-	       }
-</script>
