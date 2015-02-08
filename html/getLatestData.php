d30a65da834ac0e3887bb711844f99c26b495906
korch1337 <felixkorch1@gmail.com>
2015-02-08 23:59:04 +0100

    Update getLatestData.php

--- html/getLatestData.php
+++ html/getLatestData.php
@@ -1,18 +1,10 @@
 <div id="latestData">
-<?php require 'connect.php';
-$result = $db->query("SELECT a.name,b.prize FROM players AS a, bounty_hunters AS b WHERE a.id=b.fp_id");
-$data = array();
-
-
-while($row = $result->fetch_object()){
-    
-    $data = array(
-        'name' => $row->name,
-        'bounty' => $row->prize
+<?php
+$data = array(
+    "foo" => "bar",
+    "bar" => "foo",
     );
-    
-}
-$result->free();
+
 
 $json_data = json_encode($data);
 file_put_contents('general.json', $json_data);
