0ba0fa453082adfbe9f83ed19cff8dfa56e61f60
Nevnet1337 <davidcordesius@gmail.com>
2015-02-08 22:36:22 +0100

    Update david.php

--- html/david.php
+++ html/david.php
@@ -1,15 +1,15 @@
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
 
-<?php
-$myFile = "general.json";
-$fh = fopen($myFile, 'w') or die("can't open file");
-$stringData = $_GET["data"];
-fwrite($fh, $stringData);
-fclose($fh)
-?>
 
 
 <h1>Blank</h1>
-<p>This is a blank sample page.</p>
+<p id="test">This is a blank sample page.</p>
+
+<script>
+
+var bhunters = '{"name":"BHUNTER NAME HÄR,  }'
+
+
+</script>
 
 <?php include 'layout/overall/footer.php'; ?>
