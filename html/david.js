df5d772fe62ac6945f7d6453fcb181074d98ada8
Nevnet1337 <davidcordesius@gmail.com>
2015-02-08 21:40:30 +0100

    Update david.js

--- html/david.js
+++ html/david.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+//$(document).ready(function(){
 
-var $menu = $('ul > li > ul > li');
+//var $menu = $('ul > li > ul > li');
 
-$menu.mouseenter(function() {
-$(this).fadeTo('fast', 0.2);
-});
-$menu.mouseleave(function() {
-$(this).fadeTo('slow', 1);
+//$menu.mouseenter(function() {
+//$(this).fadeTo('fast', 0.2);
+//});
+//$menu.mouseleave(function() {
+//$(this).fadeTo('slow', 1);
   
 
-});
-});
+//});
+//});//
 
 
