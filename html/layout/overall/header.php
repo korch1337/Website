d1d109c28430bf4f272cfecf6977002d15333689
Nevnet1337 <davidcordesius@gmail.com>
2015-02-09 00:59:58 +0100

    Update header.php

--- html/layout/overall/header.php
+++ html/layout/overall/header.php
@@ -20,6 +20,8 @@ $start = $time;
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
      <script type="text/javascript" src="david.js"></script>
   <link rel="stylesheet" type="text/css" href="layout/main.css" />
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 </head>
 <body>
 
