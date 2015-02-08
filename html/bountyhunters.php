be968b80890f3c36acd1292fc32551feb3a3db76
Nevnet1337 <davidcordesius@gmail.com>
2015-02-08 21:58:13 +0100

    Update bountyhunters.php

--- html/bountyhunters.php
+++ html/bountyhunters.php
@@ -1,6 +1,6 @@
-<?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
+<?php require_once 'engine/init.php'; include 'layout/overall/header.php'; 
 
-<?php
+
 foreach($SQL->query('SELECT A.* , B.name AS hunted_by, C.name AS player_hunted, D.name AS killed_by
                         FROM bounty_hunters AS A
                         LEFT JOIN players AS B ON A.fp_id = B.id
@@ -29,5 +29,5 @@ if($num == 0){
         $main_content.='<TR BGCOLOR="'.$color.'"><TD colspan=4><center>Currently there are not any bounty hunter offer.</center></TD></TR>';
 }
     $main_content .='</TABLE></table>';
-?>
-<?php include 'layout/overall/footer.php'; ?>
+
+include 'layout/overall/footer.php'; ?>
