29d8cc22e6c7d0a773901f986d6ac890a4e36112
Nevnet1337 <davidcordesius@gmail.com>
2015-02-08 23:41:33 +0100

    Update david.php

--- html/david.php
+++ html/david.php
@@ -1,15 +1,11 @@
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
-
+<script src="david.js"></script>
 
 
 <h1>Blank</h1>
 <p id="test">This is a blank sample page.</p>
 
-<script>
-
-var bhunters = '{"name":"BHUNTER NAME HÄR,  }'
 
 
-</script>
 
 <?php include 'layout/overall/footer.php'; ?>
