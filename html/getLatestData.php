e02f49bbe1afbb7b2983fbc869cf35b38a0dd9e7
Nevnet1337 <davidcordesius@gmail.com>
2015-02-08 23:40:59 +0100

    Update getLatestData.php

--- html/getLatestData.php
+++ html/getLatestData.php
@@ -1,4 +1,16 @@
 <div id="latestData">
+<?php
+$result = mysql_query(your sql here);    
+$data = array();
+while ($row = mysql_fetch_assoc($result)) {
+    // Generate the output in desired format
+    $data = array(
+        'name' => ....
+        'bounty' => ....
+    );
+}
 
-
+$json_data = json_encode($data);
+file_put_contents('general.json', $json_data);
+?>
 </div>
