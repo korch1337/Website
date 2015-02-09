0f5cdf47c6cea4a8e5a685c3591e3367dad8aa02
Nevnet1337 <davidcordesius@gmail.com>
2015-02-09 01:39:58 +0100

    Update david.js

--- html/david.js
+++ html/david.js
@@ -8,7 +8,6 @@ $.ajax({
         type: 'GET',
         url: 'general.json',
         dataType: 'json',
-        async: false,
         success: function (bounty) {
             $.each(bounty, function(i, bounty){
                 $bountyh.append('<li> Name: ' + bounty.name + ', Bounty: ' + bounty.price + '</li>');
