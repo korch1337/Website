36510cd7dd98001f14e665b9987cb41a579f1280
Nevnet1337 <davidcordesius@gmail.com>
2015-02-09 04:29:13 +0100

    Update david.js

--- html/david.js
+++ html/david.js
@@ -9,12 +9,16 @@ $.ajax({
         url: 'general.json',
         dataType: 'json',
         success: function (bounty) {
- $.each(bounty, function(i, bounties){
-         $.each(bounty[i], function(i2, bounties){
-                 $.each(bounty[i2], function(i2, bounties){
-                 $('#test').append('<li id="remove"> Name: ' + bounty[i].name + ', Bounty: ' + bounty[i].prize + '</li>');  });
- });
- });
+ //$.each(bounty, function(i, bounties){
+        // $.each(bounty[i], function(i2, bounties){
+            //     $('#test').append('<li id="remove"> Name: ' + bounty[i].name + ', Bounty: ' + bounty[i].prize + '</li>'); 
+               //  });
+
+// });
+        for (i=0; i < bounty.length / 2; i++){
+                $('#test').append('<li id="remove"> Name: ' + bounty[i].name + ', Bounty: ' + bounty[i].prize + '</li>')
+                
+        };
         }
         
 });
