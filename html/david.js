f6beaccc993e5ebd3696698188065f97be8f84b5
korch1337 <felixkorch1@gmail.com>
2015-02-09 21:58:23 +0100

    Update david.js

--- html/david.js
+++ html/david.js
@@ -1,22 +1,17 @@
 $(document).ready(function(){
 setInterval(ajaxd,1000);
 });
-var testing= [];
 function ajaxd(){
-        
 $.ajax({
         type: 'GET',
         url: 'general.json',
         dataType: 'json',
         async: true,
         success: function (bounty) {
-                if (testing.length != bounty.length){
-                        var testing = bounty;
-                        $('#test').empty();
-        for (i=0; i <= bounty.length - 1; i++){
-                $('#test').append('<li id="remove"> Name: ' + bounty[i].name + ', Bounty: ' + bounty[i].prize + '</li>')
-        };
-                }
+                $('#test').empty();
+                for (i=0; i <= bounty.length - 1; i++){
+                        $('#test').append('<li id="remove"> Name: ' + bounty[i].name + ', Bounty: ' + bounty[i].prize + '</li>')
+                };
         }
         
 });
