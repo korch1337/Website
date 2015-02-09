5469db2368decb99924ea5160bd3e1a9e8c12174
korch1337 <felixkorch1@gmail.com>
2015-02-09 15:35:32 +0100

    Update david.js

--- html/david.js
+++ html/david.js
@@ -5,7 +5,7 @@ setInterval(ajaxd,1000);
 function ajaxd(){
 $.ajax({
         type: 'GET',
-        url: 'getLatestData.php',
+        url: 'general.json',
         dataType: 'json',
         async: true,
         success: function (bounty) {
