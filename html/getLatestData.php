f0b7e635fdacf14336621182cfc9e545f7aabe3c
Nevnet1337 <davidcordesius@gmail.com>
2015-02-09 02:58:22 +0100

    Update getLatestData.php

--- html/getLatestData.php
+++ html/getLatestData.php
@@ -1,7 +1,7 @@
 <?php require 'connect.php';
 
-$json = file_get_contents("general.json");
-$bounties = json_decode($json, true);
+// $json = file_get_contents("general.json");
+// $bounties = json_decode($json, true);
 
 $result = $db->query("SELECT a.name,b.prize FROM players AS a, bounty_hunters AS b WHERE a.id=b.fp_id");
 
