c8aa9e7e8acd14a72da1c98697054bafd773e3e1
Nevnet1337 <davidcordesius@gmail.com>
2015-02-09 19:18:45 +0100

    Update david.php

--- html/david.php
+++ html/david.php
@@ -1,5 +1,6 @@
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
 <script src="david.js"></script>
+<script src="davidphp.js"></script>
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
@@ -7,6 +8,7 @@
   <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
 
+<div></div>
 
 <h1>Blank</h1>
 <p >This is a blank sample page.</p>
