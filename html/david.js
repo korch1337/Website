58250b0637264ff27ab1e88fb8da41b65349e0c0
korch1337 <felixkorch1@gmail.com>
2015-02-09 04:35:15 +0100

    Update david.js

--- html/david.js
+++ html/david.js
@@ -8,9 +8,8 @@ $.ajax({
         url: 'general.json',
         dataType: 'json',
         success: function (bounty) {
-        for (i=0; i <= 2;){
+        for (i=0; i <= 1;){
                 $('#test').append('<li id="remove"> Name: ' + bounty[i].name + ', Bounty: ' + bounty[i].prize + '</li>')
-                i++;
         };
         }
         
