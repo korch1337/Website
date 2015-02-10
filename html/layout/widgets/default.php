0f62eab410c218b1cb79d4dd717944e3d11e15a7
korch1337 <felixkorch1@gmail.com>
2015-02-10 21:08:19 +0100

    Update default.php

--- html/layout/widgets/default.php
+++ html/layout/widgets/default.php
@@ -1,11 +1,4 @@
 
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="layout/main.css" />
-<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
-
      <div class="sidebar">
         <!-- insert your sidebar items here -->
         <h3>Character Search</h3>
