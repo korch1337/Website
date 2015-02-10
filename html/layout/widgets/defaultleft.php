0325a691dac8bf128dba312a301a9c1a1da0332b
korch1337 <felixkorch1@gmail.com>
2015-02-10 20:01:59 +0100

    Update defaultleft.php

--- html/layout/widgets/defaultleft.php
+++ html/layout/widgets/defaultleft.php
@@ -1,1 +1,12 @@
-a
+<div class="sidebar">
+        <!-- insert your sidebar items here -->
+        <h3>Server Information</h3>
+        <!-- server information goes here -->
+		<br>
+          <br>
+          <h3>Top 5 worst players</h3>
+          <p>1. asdads<br>2. asdsad</p>
+        
+         <br>
+          <h3>Most Powerful Guilds:</h3>
+        </div>
