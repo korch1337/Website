fe4da07a01946e5679f71dae845352059d91f55e
korch1337 <felixkorch1@gmail.com>
2015-02-11 02:06:38 +0100

    Update footer.php

--- html/layout/overall/footer.php
+++ html/layout/overall/footer.php
@@ -29,7 +29,7 @@
         <div class="header"><a href="#">latest media</a></div>
         <div class="body">
           <ul id="l_media_list">
-            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/full/demon.jpg" /></a></li>
+            <li><a class="shadowbox" href="layout/images/media/full/demon.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/demon.jpg" /></a></li>
             <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
             <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
             <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
