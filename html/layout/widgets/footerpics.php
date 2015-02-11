94401cb7f5935c06e94c43abf677a929aa38a999
korch1337 <felixkorch1@gmail.com>
2015-02-11 19:01:32 +0100

    Create footerpics.php

---
+++ html/layout/widgets/footerpics.php
@@ -0,0 +1,12 @@
+<ul id="l_media_list">
+            <li><a class="shadowbox" href="layout/images/media/full/demon.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/demon.jpg" /></a></li>
+            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
+            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
+            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
+            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
+            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
+            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
+            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
+            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
+            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
+          </ul>
