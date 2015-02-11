5939296101b0a5ba096c525c360c0cd7becd0a6c
korch1337 <felixkorch1@gmail.com>
2015-02-11 19:24:52 +0100

    Update characterprofile.php

--- html/characterprofile.php
+++ html/characterprofile.php
@@ -729,7 +729,7 @@ if ($PEQD !== false) {
 <tr><td>Search Character</td></tr>
 <tr class="darkborder"><td>
 	<form type="submit" action="characterprofile.php" method="get">
-		Name: <br><input type="text" size="25" name="name" class="search">
+		Name: <br><input type="text" size="25" name="name" class="search"><br>
 		      <input type="submit" name="submitName" value="Submit"> 
 	</form>
 </td></tr>
