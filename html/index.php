d736f3e1d1c20d13916ff3dad9e9dce4ba1592d1
korch1337 <felixkorch1@gmail.com>
2015-02-11 19:02:39 +0100

    Update index.php

--- html/index.php
+++ html/index.php
@@ -352,18 +352,7 @@
       <div id="latest_media">
         <div class="header"><a href="#">latest media</a></div>
         <div class="body">
-          <ul id="l_media_list">
-            <li><a class="shadowbox" href="layout/images/media/full/demon.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/demon.jpg" /></a></li>
-            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
-            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
-            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
-            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
-            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
-            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
-            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
-            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
-            <li><a class="shadowbox" href="layout/images/media/full/1.jpg" rel="gallery" ><img alt="alt_example" src="layout/images/media/thumb/1.jpg" /></a></li>
-          </ul>
+          <?php include 'layout/widgets/footerpics.php'; ?>
         </div>
       </div>
       <div class="clear"></div>
