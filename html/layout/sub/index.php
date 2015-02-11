ad2525c81456e99808d4178a306b91ffa05d9fa0
Felix Korch <felixkorch1@gmail.com>
2015-02-11 01:37:18 +0100

    index

--- html/layout/sub/index.php
+++ html/layout/sub/index.php
@@ -1,235 +1,85 @@
-<div id="da-slider" class="da-slider">
-      <div class="da-slide">
-        <h2><a href="#" class="da-link">For gamer by gamers</a></h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
-          Ut iaculis lorem vitae arcu elementum pellentesque. <br />
-          Praesent pellentesque ornare neque id lobortis.</p>
-        <div class="da-img"><img alt="alt_example" src="./images/paralax_banner/3.png"  /></div>
-      </div>
-      <div class="da-slide">
-        <h2><a href="#" class="da-link">Blade and Soul</a></h2>
-        <p>Phasellus ac leo turpis. Morbi at pulvinar augue. <br />
-          Aenean rhoncus ultrices volutpat. Vivamus eget enim ut orci iaculis condimentum sed a quam. </p>
-        <div class="da-img"><img alt="alt_example" src="./images/paralax_banner/2.png"  /></div>
-      </div>
-      <div class="da-slide">
-        <h2><a href="#" class="da-link">Final Fantasy XIV</a></h2>
-        <p>Etiam eu massa lectus. Nunc mi velit, commodo ut ullamcorper et, consectetur vel dolor. Etiam tincidunt convallis metus non suscipit.</p>
-        <div class="da-img"><img alt="alt_example" src="./images/paralax_banner/1.png"  /></div>
-      </div>
-      <div class="da-arrows"> <span class="da-arrows-prev"></span> <span class="da-arrows-next"></span> </div>
-    </div>
-    
-    <!--********************************************* Banner end *********************************************-->
-    
-    <div class="top_shadow"></div>
-    
-    <!--********************************************* Hot news start *********************************************-->
-    <div id="hot_news">
-      <div class="header">
-        <h1><span>ORIZON //</span> HOT NEWS</h1>
-      </div>
-      
-      <!-- Previous and next selector --> 
-      <a id="prev" class="prev" href="#"><img alt="alt_example" src="./images/blank.gif" width="21" height="33" border="0" /></a> <a id="next" class="next" href="#"><img alt="alt_example" src="./images/blank.gif" width="21" height="33" border="0" /></a>
-      <ul id="hot_news_box">
-        <li>
-          <h2><a href="./post.html">Battlefield 3 expansion!</a></h2>
-          <div class="image"><a href="./post.html"><img alt="alt_example" src="./images/media/full/1.jpg"/></a></div>
-          <div class="content">
-            <p>Nam dignissim nulla mattis justo aliquet luctus. Mauris venenatis eros. Nam in leo libero. In hac habitasse platea dictumst. Phasellus aliquet aliquam sto,dignissi
-              eistoteles anarequi et son amorites etimo nurli.</p>
-            <div class="info"> <a href="./post.html" class="comments">18 Comments</a> <a href="./post.html" class="read_more">read more</a> </div>
-          </div>
-        </li>
-        <li>
-          <h2><a href="./post.html">Dead space looking good</a></h2>
-          <div class="image"><a href="./post.html"><img alt="alt_example" src="./images/media/full/1.jpg" /></a></div>
-          <div class="content">
-            <p>Nam dignissim nulla mattis justo aliquet luctus. Mauris venenatis eros. Nam in leo libero. In hac habitasse platea dictumst. Phasellus aliquet aliquam sto,dignissi
-              eistoteles anarequi et son amorites etimo nurli.</p>
-            <div class="info"> <a href="./post.html" class="comments">18 Comments</a> <a href="./post.html" class="read_more">read more</a> </div>
-          </div>
-        </li>
-        <li>
-          <h2><a href="./post.html">Dead space new footage</a></h2>
-          <div class="image"><a href="#"><img alt="alt_example" src="./images/media/full/1.jpg" /></a></div>
-          <div class="content">
-            <p>Nam dignissim nulla mattis justo aliquet luctus. Mauris venenatis eros. Nam in leo libero. In hac habitasse platea dictumst. Phasellus aliquet aliquam sto,dignissi
-              eistoteles anarequi et son amorites etimo nurli.</p>
-            <div class="info"> <a href="./post.html" class="comments">18 Comments</a> <a href="./post.html" class="read_more">read more</a> </div>
-          </div>
-        </li>
-        <li>
-          <h2><a href="./post.html">Owning at DayZ</a></h2>
-          <div class="image"><a href="#"><img alt="alt_example" src="./images/media/full/1.jpg" /></a></div>
-          <div class="content">
-            <p>Nam dignissim nulla mattis justo aliquet luctus. Mauris venenatis eros. Nam in leo libero. In hac habitasse platea dictumst. Phasellus aliquet aliquam sto,dignissi
-              eistoteles anarequi et son amorites etimo nurli.</p>
-            <div class="info"> <a href="./post.html" class="comments">18 Comments</a> <a href="./post.html" class="read_more">read more</a> </div>
-          </div>
-        </li>
-        <li>
-          <h2><a href="./post.html">LoL tournament </a></h2>
-          <div class="image"><a href="#"><img alt="alt_example" src="./images/media/full/1.jpg" /></a></div>
-          <div class="content">
-            <p>Nam dignissim nulla mattis justo aliquet luctus. Mauris venenatis eros. Nam in leo libero. In hac habitasse platea dictumst. Phasellus aliquet aliquam sto,dignissi
-              eistoteles anarequi et son amorites etimo nurli.</p>
-            <div class="info"> <a href="./post.html" class="comments">18 Comments</a> <a href="./post.html" class="read_more">read more</a> </div>
-          </div>
-        </li>
-        <li>
-          <h2><a href="./post.html">MOHW not as good as planned</a></h2>
-          <div class="image"><a href="#"><img alt="alt_example" src="./images/media/full/1.jpg" /></a></div>
-          <div class="content">
-            <p>Nam dignissim nulla mattis justo aliquet luctus. Mauris venenatis eros. Nam in leo libero. In hac habitasse platea dictumst. Phasellus aliquet aliquam sto,dignissi
-              eistoteles anarequi et son amorites etimo nurli.</p>
-            <div class="info"> <a href="./post.html" class="comments">18 Comments</a> <a href="./post.html" class="read_more">read more</a> </div>
-          </div>
-        </li>
-      </ul>
-    </div>
-    <!--********************************************* Hot news end *********************************************--> 
-    
-    <!--********************************************* Main start *********************************************-->
-    <div id="main_news_wrapper">
- 
-       <div id="row"> 
-        <!-- Left wrapper Start -->
-        <div id="left_wrapper">
-        <div class="header">
-            <h2><span>ORIZON //</span> GENERAL NEWS</h2>
-          </div>
-          <ul id="general_news">
-            <li>
-              <div class="image"><a href="./post.html"><img alt="alt_example" src="./images/media/full/1.jpg" /></a></div>
-              <ul class="social_share">
-                <li><a href="#"><img alt="alt_example" src="./images/fbk.png" border="0" /></a></li>
-                <li><a href="#"><img alt="alt_example" src="./images/twitter.png" border="0" /></a></li>
-                <li><a href="#"><img alt="alt_example" src="./images/more.png" border="0" /></a></li>
-              </ul>
-              <div class="info">
-                <div class="comments"> 18 </div>
-                <h2><a href="./post.html">Blade and Soul - Open beta announced!</a></h2>
-                <div class="date_n_author">12 July 2012, by Admin</div>
-                <p>Integer tincidunt tellus ut metus viverra ac sodales odio vulputate. Ut id erat neque, sit amet tristique erat. Vivamus in turpis orci, et volutpat purus. Vestibulum lacinia, arcu id varius eleifend, eros lorem iaculis massa, vitae consequat enim diam eget massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam ultrices viverra consectetur. Sed eget massa augue. Suspendisse urna lectus.</p>
-                <a href="./post.html" class="read_more2">read more</a> </div>
-                <div class="clear">
-              </div>
-            </li>
-            <li>
-              <div class="image"><a href="./post.html"><img alt="alt_example" src="./images/media/full/1.jpg" /></a></div>
-              <ul class="social_share">
-                <li><a href="#"><img alt="alt_example" src="./images/fbk.png" border="0" /></a></li>
-                <li><a href="#"><img alt="alt_example" src="./images/twitter.png" border="0" /></a></li>
-                <li><a href="#"><img alt="alt_example" src="./images/more.png" border="0" /></a></li>
-              </ul>
-              <div class="info">
-                <div class="comments"> 18 </div>
-                <h2><a href="./post.html">Raiderz item giveaway!</a></h2>
-                <div class="date_n_author">12 July 2012, by Admin</div>
-                <p>Integer tincidunt tellus ut metus viverra ac sodales odio vulputate. Ut id erat neque, sit amet tristique erat. Vivamus in turpis orci, et volutpat purus. Vestibulum lacinia, arcu id varius eleifend, eros lorem iaculis massa, vitae consequat enim diam eget massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam ultrices viverra consectetur. Sed eget massa augue. Suspendisse urna lectus.</p>
-                <a href="./post.html" class="read_more2">read more</a> </div>
-                <div class="clear"></div>
-            </li>
-            <li>
-              <div class="image"><a href="./post.html"><img alt="alt_example" src="./images/media/full/1.jpg" /></a></div>
-              <ul class="social_share">
-                <li><a href="#"><img alt="alt_example" src="./images/fbk.png" border="0" /></a></li>
-                <li><a href="#"><img alt="alt_example" src="./images/twitter.png" border="0" /></a></li>
-                <li><a href="#"><img alt="alt_example" src="./images/more.png" border="0" /></a></li>
-              </ul>
-              <div class="info">
-                <div class="comments"> 18 </div>
-                <h2><a href="./post.html">Dead Space 3 gameplay</a></h2>
-                <div class="date_n_author">12 July 2012, by Admin</div>
-                <p>Integer tincidunt tellus ut metus viverra ac sodales odio vulputate. Ut id erat neque, sit amet tristique erat. Vivamus in turpis orci, et volutpat purus. Vestibulum lacinia, arcu id varius eleifend, eros lorem iaculis massa, vitae consequat enim diam eget massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam ultrices viverra consectetur. Sed eget massa augue. Suspendisse urna lectus.</p>
-                <a href="./post.html" class="read_more2">read more</a> </div>
-                <div class="clear"></div>
-            </li>
-          </ul>
-          <ul id="pager">
-            <li><a href="#" ><img alt="alt_example" src="./images/left_pager.jpg" border="0"/></a></li>
-            <li><a href="#" >1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#" class="active">3</a></li>
-            <li><a href="#"><img alt="alt_example" src="./images/right_pager.jpg" border="0"/></a></li>
-          </ul>
-          
-          
-          <div class="clear"></div>
-          </div>
-        <!-- Left wrapper end --> 
-        
-        <!-- Right wrapper Start -->
-        <div id="right_wrapper">
-          <div id="search">
-            <input type="text" onblur="if(this.value =='') this.value='search'" onfocus="if (this.value == 'search') this.value=''" value="search" name="s" class="required" id="s" />
-            <input type="button" />
-          </div>
-          <div class="review">
-            <div class="header"><a href="#">Top games</a></div>
-            <ul>
-              <li>
-                <div class="img"><a href="./post_game.html"><img alt="alt_example" src="./images/media/thumb/1.jpg" /></a></div>
-                <div class="info"> <a href="./post_game.html">Dead space 3</a><br/>
-                  <small>05 December 2011, 2 Comments</small><br/>
-                  <img alt="alt_example" src="./images/stars.png" /> </div>
-              </li>
-              <li>
-                <div class="img"><a href="./post_game.html"><img alt="alt_example" src="./images/media/thumb/1.jpg" /></a></div>
-                <div class="info"> <a href="./post_game.html">Battlefield 3</a><br/>
-                  <small>05 December 2011, 2 Comments</small><br/>
-                  <img alt="alt_example" src="./images/stars.png" /> </div>
-              </li>
-              <li>
-                <div class="img"><a href="./post_game.html"><img alt="alt_example" src="./images/media/thumb/1.jpg" /></a></div>
-                <div class="info"> <a href="./post_game.html">DayZ - Arma 2 mod</a><br/>
-                  <small>05 December 2011, 2 Comments</small><br/>
-                  <img alt="alt_example" src="./images/stars.png" /> </div>
-              </li>
-              <li>
-                <div class="img"><a href="./post_game.html"><img alt="alt_example" src="./images/media/thumb/1.jpg" /></a></div>
-                <div class="info"> <a href="./post_game.html">League of legends</a><br/>
-                  <small>05 December 2011, 2 Comments</small><br/>
-                  <img alt="alt_example" src="./images/stars.png" /> </div>
-              </li>
-            </ul>
-          </div>
-          
-          
-          <div class="advert">
-            <a href="http://themeforest.net/user/Skywarrior" target="_blank"><img alt="alt_example" src="./images/advert_r.jpg" border="0" /></a>
-          </div>
-          
-          
-          <div class="categories">
-            <div class="header"><a href="#">Categories</a></div>
-            <ul>
-              <li> <a href="./post_list.html">Action</a> </li>
-              <li> <a href="./post_list.html">Adventure</a> </li>
-              <li> <a href="./post_list.html">Strategy</a> </li>
-              <li> <a href="./post_list.html">RPG</a> </li>
-              <li> <a href="./post_list.html">Indie</a> </li>
-              <li> <a href="./post_list.html">Massive Multiplayer</a> </li>
-            </ul>
-          </div>
-                 
-      
-      <!-- Right wrapper end -->
-      
-        </div>
-      	<div class="clear"></div>
-        
-        </div>
-      </div>
-    
-    <div class="bottom_shadow"></div>
+<table>
+
+<?php
+$cache = new Cache('engine/cache/news');
+if ($cache->hasExpired()) {
+	$news = fetchAllNews();
+	
+	$cache->setContent($news);
+	$cache->save();
+} else {
+	$news = $cache->load();
+}
+// Design and present the list
+if ($news) {
+	function TransformToBBCode($string) {
+		$tags = array(
+			'[center]{$1}[/center]' => '<center>$1</center>',
+			'[b]{$1}[/b]' => '<b>$1</b>',
+			'[size={$1}]{$2}[/size]' => '<font size="$1">$2</font>',
+			'[img]{$1}[/img]'    => '<a href="$1" target="_BLANK"><img src="$1" alt="image" style="width: 100%"></a>',
+			'[link]{$1}[/link]'    => '<a href="$1">$1</a>',
+			'[link={$1}]{$2}[/link]'   => '<a href="$1" target="_BLANK">$2</a>',
+			'[color={$1}]{$2}[/color]' => '<font color="$1">$2</font>',
+			'[*]{$1}[/*]' => '<li>$1</li>',
+		);
+		foreach ($tags as $tag => $value) {
+			$code = preg_replace('/placeholder([0-9]+)/', '(.*?)', preg_quote(preg_replace('/\{\$([0-9]+)\}/', 'placeholder$1', $tag), '/'));
+			$string = preg_replace('/'.$code.'/i', $value, $string);
+		}
+		return $string;
+	}
+	echo '<div id="news">'; ?>
+	<?php
+	// Most powerful guilds for TFS 0.3/4 and 1.0
+////////////////////////
+// Create a cache file to avoid high SQL load
+$cache = new Cache('engine/cache/guilds');
+if ($cache->hasExpired()) {
+    // Fetch guild data
   
-    <!--********************************************* Main end *********************************************--> 
-    
-    <!--********************************************* Main advert start *********************************************-->
-    <div class="main_advert">
-      <a href="http://themeforest.net/user/Skywarrior" target="_blank"><img alt="alt_example" src="./images/main_ad.png" border="0" /></a>
-      
+if ($config['TFSVersion'] == 'TFS_03') $guilds = mysql_select_multi('SELECT `g`.`id` AS `id`, `g`.`name` AS `name`, COUNT(`g`.`name`) as `frags` FROM `killers` k LEFT JOIN `player_killers` pk ON `k`.`id` = `pk`.`kill_id` LEFT JOIN `players` p ON `pk`.`player_id` = `p`.`id` LEFT JOIN `guild_ranks` gr ON `p`.`rank_id` = `gr`.`id` LEFT JOIN `guilds` g ON `gr`.`guild_id` = `g`.`id` WHERE `k`.`unjustified` = 1 AND `k`.`final_hit` = 1 GROUP BY `name` ORDER BY `frags` DESC, `name` ASC LIMIT 0, 3;');
+elseif ($config['TFSVersion'] == 'TFS_10') $guilds = mysql_select_multi('SELECT `g`.`id` AS `id`, `g`.`name` AS `name`, COUNT(`g`.`name`) as `frags` FROM `players` p LEFT JOIN `player_deaths` pd ON `pd`.`killed_by` = `p`.`name` LEFT JOIN `guild_membership` gm ON `p`.`id` = `gm`.`player_id` LEFT JOIN `guilds` g ON `gm`.`guild_id` = `g`.`id` WHERE `pd`.`unjustified` = 1 GROUP BY `name` ORDER BY `frags` DESC, `name` ASC LIMIT 0, 3;');
+    $cache->setContent($guilds);
+    $cache->save();
+} else {
+    $guilds = $cache->load();
+}
+if (!empty($guilds) || !$guilds) {
+    $divsize = 400;
+    ?>
+    <!-- No table design -->
+    <center><h1>Most powerful guilds</h1></center>
+    <div style="margin: auto; width: <?php echo $divsize; ?>px;">
+        <?php
+        $number = 1;
+        foreach ($guilds as $guild) {
+            ?>
+            <div style="float: left; width: <?php echo (int)$divsize / 3; ?>px;">
+                <a href="guilds.php?name=<?php echo $guild['name']; ?>"><img style="max-width: <?php echo (int)$divsize / 3; ?>px;" src="medals/<?php echo $number; ?>.png" alt="<?php echo $number; ?>"><br>
+                <center><b><?php echo $guild['name']; ?></b><br>
+                Kills: <?php echo $guild['frags']; ?></center></a>
+            </div>
+            <?php
+            $number++;
+        }
+        ?>
     </div>
+    <!-- With table design -->
+    <?php
+}
+// End powerful guilds
+	foreach ($news as $n) {
+		?>
+<tr><td class="znewsdate"><?php echo date('l, d M Y', $n['date']); ?></td></tr>
+<tr><td class="zheadline">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b><?php echo TransformToBBCode($n['title']); ?></td></tr>
+<tr><td class="znewsbody" colspan="2"><?php echo TransformToBBCode(nl2br($n['text'])); ?></td></tr>
+<tr><td class="znewsdate">by <a href="characterprofile.php?name=<?php echo $n['name']; ?>"><?php echo $n['name']; ?></a></td><td></td></tr>
+<tr><td class="znewsdate" style="border-bottom:1px solid #000000;" colspan="2"></td></tr>
+<tr><td class="znewsdate" colspan="2"></td></tr>
+		<?php
+	}
+	echo '</div>';
+}
+?>
+</table>
