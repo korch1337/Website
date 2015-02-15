680bf640f1095c7b8e6edcbd6ad15c606cf5c4ff
korch1337 <felixkorch1@gmail.com>
2015-02-16 00:42:54 +0100

    Update index.php

--- html/index.php
+++ html/index.php
@@ -100,6 +100,17 @@
     <div id ="charsearch">
     <?php include 'charactersearch.php'; ?>
     </div>
+    <form action="login.php" method="post">
+				&nbsp;<i style="font-size:11px">Account number:</i> <br>
+			<input type="text" name="username"> <br>
+				&nbsp;<i style="font-size:11px">Password:</i> <br>
+			<input type="password" name="password">
+			<br><br><input type="submit" value="Log in">
+			<?php
+				/* Form file */
+				Token::create();
+			?>
+		</form>
     
     <!--********************************************* Banner start *********************************************-->
     <div id="da-slider" class="da-slider">
