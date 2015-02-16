01afe60fa1d2030561d5d85a6e9aacb7226e0754
korch1337 <felixkorch1@gmail.com>
2015-02-16 01:35:07 +0100

    Update contact.php

--- html/contact.php
+++ html/contact.php
@@ -1,6 +1,6 @@
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
 
 <h1>Contact</h1>
-<p>TODO: Edit the contact details here.</p>
+<p>Contact us at: admin@zerexxa.net</p>
 
 <?php include 'layout/overall/footer.php'; ?>
