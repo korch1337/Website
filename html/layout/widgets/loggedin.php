cdb0dc88826d5fda97b37a48f6f325e27cdec550
korch1337 <felixkorch1@gmail.com>
2015-02-16 02:20:59 +0100

    Update loggedin.php

--- html/layout/widgets/loggedin.php
+++ html/layout/widgets/loggedin.php
@@ -3,6 +3,7 @@
 		  <li><a href="myaccount.php">Account Page</a></li>
 		  <li><a href='redeem.php'>Redeem a coupon</a></li>
 		  <li><a href="createcharacter.php">Create Character</a></li>
+		  <li><a href="sellcharacter.php">Sell Character</a></li>
 		  <li><a href="settings.php">Settings</a></li>
 		  <li><a href="logout.php">Log out</a></li>
 		  </ul>
